01f1dfb469412cea597a4f1808774217ebd6f21f
Anand Bharadwaj <anand@zoolore.org>
2018-03-20 15:44:13 +0530

    new

---
+++ test_driver/demo.test.dart
@@ -0,0 +1,10 @@
+import 'dart:async';
+
+// Imports the Flutter Driver API
+import 'package:flutter_driver/flutter_driver.dart';
+import 'package:test/test.dart';
+
+// ignore: expected_token
+void main() {
+
+}
