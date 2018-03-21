52c115e7c0cc61902acad87cb100accca7ddf107
Anand Bharadwaj <anand@zoolore.org>
2018-03-21 23:21:12 +0530

    test
    updated script to navigate to true or false game

--- test_driver/user_list_test.dart
+++ test_driver/user_list_test.dart
@@ -24,9 +24,10 @@ void main() {
       final SerializableFinder game = find.text('Game');
       await driver.tap(game);
 
-      final SerializableFinder trueandfalse = find.byValueKey('memory');
-      await driver.tap(trueandfalse);
-
+      final SerializableFinder memory = find.byValueKey('memory');
+      await driver.scroll(memory, 0.0, -600.0, const Duration(milliseconds: 300));
+      final SerializableFinder true_or_false = find.byValueKey('true_or_false');
+      await driver.tap(true_or_false);
       final SerializableFinder mode = find.byValueKey('single');
       await driver.tap(mode);
 
