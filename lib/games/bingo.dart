760f766fc063d03f9a44899d22da2c0071d4d014
kiran kumar bv <kiran.bv@zoolore.org>
2018-03-21 10:51:43 +0530

    bingo game

---
+++ lib/games/bingo.dart
@@ -0,0 +1,184 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
+import 'dart:ui' show window;
+
+class Bingo extends StatefulWidget {
+  Function onScore;
+  Function onProgress;
+
+  Bingo({key, this.onScore, this.onProgress}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => new BingoState();
+}
+
+class BingoState extends State<Bingo> {
+  final List<String> _allLetters = [
+//    'A',
+//    'B',
+//    'C',
+//    'D',
+//    'E',
+//    'F',
+//    'G',
+//    'H',
+//    'I',
+//    'J',
+//    'K',
+//    'L',
+//    'M',
+//    'N',
+//    'O',
+//    'P',
+//    'Q',
+//    'R',
+//    'S',
+//    'T',
+//    'U',
+//    'V',
+//    'W',
+//    'X',
+//    'Y',
+//    'Z'
+    '1','2','3','4','5','6','7','8','9' ,'1','2','3','4','5','6','7','8','9'
+  ];
+  final List<String> question = ['1 + 2','2 + 3','1 + 5' ];
+  final int _size = 4;
+  var _currentIndex = 0;
+  List<String> _shuffledLetters = [];
+  List<String> _letters;
+
+  @override
+  void initState() {
+    super.initState();
+    for (var i = 0; i < _allLetters.length; i += _size * _size) {
+      _shuffledLetters.addAll(
+          _allLetters.skip(i).take(_size * _size).toList(growable: false)
+            ..shuffle());
+    }
+    print(_shuffledLetters);
+    _letters = _shuffledLetters.sublist(0, _size * _size);
+  }
+
+  Widget _buildItem(int index, String text) {
+    return new MyButton(
+        key: new ValueKey<int>(index),
+        text: text,
+        onPress: () {
+          if (text == _allLetters[_currentIndex]) {
+            setState(() {
+              _letters[index] =
+              _size * _size + _currentIndex < _allLetters.length
+                  ? _shuffledLetters[_size * _size + _currentIndex]
+                  : "";
+              _currentIndex++;
+            });
+            widget.onScore(1);
+            widget.onProgress(_currentIndex/_allLetters.length);
+          }
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print("MyTableState.build");
+    MediaQueryData media = MediaQuery.of(context);
+    print(media);
+    List<TableRow> rows = new List<TableRow>();
+    var j = 0;
+    for (var i = 0; i < _size; ++i) {
+      List<Widget> cells = _letters
+          .skip(i * _size)
+          .take(_size)
+          .map((e) => _buildItem(j++, e))
+          .toList();
+      rows.add(new TableRow(children: cells));
+    }
+//    return new Table(children: rows);
+    return new Container(
+      child: new Column(
+        children: <Widget>[
+          new Container
+            (color: Colors.orange,height: 60.0,width: 70.0,
+              child:new Center(child:new Text("$question", style: new TextStyle(color: Colors.black, fontSize: 30.0)))),
+          new Table(children: rows),
+//          new Container
+//            (color: Colors.orange,height: 60.0,
+//              child:new Center(child:new Text("num3", style: new TextStyle(color: Colors.black, fontSize: 30.0)))),
+        ],
+      ),
+    );
+  }
+}
+
+class MyButton extends StatefulWidget {
+  MyButton({Key key, this.text, this.onPress}) : super(key: key);
+
+  final String text;
+  final VoidCallback onPress;
+
+  @override
+  _MyButtonState createState() => new _MyButtonState();
+}
+
+class _MyButtonState extends State<MyButton> with TickerProviderStateMixin {
+  AnimationController controller;
+  Animation<double> animation;
+  String _displayText;
+
+  initState() {
+    super.initState();
+    print("_MyButtonState.initState: ${widget.text}");
+    _displayText = widget.text;
+    controller = new AnimationController(
+        duration: new Duration(milliseconds: 250), vsync: this);
+    animation = new CurvedAnimation(parent: controller, curve: Curves.easeIn)
+      ..addStatusListener((state) {
+        print("$state:${animation.value}");
+        if (state == AnimationStatus.completed) {
+          print('dismissed');
+          if (!widget.text.isEmpty) {
+            setState(() => _displayText = widget.text);
+            controller.forward();
+          }
+        }
+      });
+    controller.forward();
+  }
+
+  @override
+  void didUpdateWidget(MyButton oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.text != widget.text) {
+      controller.reverse();
+    }
+    print("_MyButtonState.didUpdateWidget: ${widget.text} ${oldWidget.text}");
+  }
+
+  void _handleTouch() {
+    print(widget.text);
+    controller.reverse();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print("_MyButtonState.build");
+
+
+      return new TableCell(
+        child: new Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: new ScaleTransition(
+                scale: animation,
+                child: new RaisedButton(
+                    onPressed: () => widget.onPress(),
+                    padding: const EdgeInsets.all(8.0),
+                    color: Colors.teal,
+                    shape: new RoundedRectangleBorder(
+                        borderRadius:
+                        const BorderRadius.all(const Radius.circular(8.0))),
+                    child: new Text(_displayText,
+                        style: new TextStyle(
+                            color: Colors.white, fontSize: 24.0))))));
+  }
+}
