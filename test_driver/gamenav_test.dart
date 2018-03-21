a4778f1303835e0c8677cf9191380c56a00a9ddb
Anand Bharadwaj <anand@zoolore.org>
2018-03-21 11:54:03 +0530

    latest

---
+++ test_driver/gamenav_test.dart
@@ -0,0 +1,30 @@
+import 'dart:async';
+
+import 'package:flutter_driver/flutter_driver.dart';
+import 'package:test/test.dart';
+
+const Duration kWaitBetweenActions = const Duration(milliseconds: 1000);
+
+void main() {
+  group('login', () {
+    FlutterDriver driver;
+    setUpAll(() async {
+      driver = await FlutterDriver.connect();
+    });
+
+//    tearDownAll(() async {
+//      if (driver != null)
+//        await driver.close();
+//    });
+
+    test('add a user', () async {
+      final Completer<Null> completer = new Completer<Null>();
+      final SerializableFinder game = find.text('Game');
+      await driver.tap(game);
+      final SerializableFinder story = find.text('Story');
+      await driver.tap(story);
+      completer.complete();
+      await completer.future;
+    }, timeout: const Timeout(const Duration(minutes: 1)));
+  });
+}
