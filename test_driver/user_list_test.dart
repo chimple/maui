e81049ebba438c0d9bd9b8636c954fba1fd6fcb1
Anand Bharadwaj <anand@zoolore.org>
2018-03-22 16:22:25 +0530

    updated

--- test_driver/user_list_test.dart
+++ test_driver/user_list_test.dart
@@ -17,17 +17,29 @@ void main() {
         await driver.close();
     });
 
-    test('task', () async {
+    test('signin', () async {
+
       final Completer<Null> completer = new Completer<Null>();
+      await new Future<Duration>.delayed(const Duration(milliseconds: 200));
+
       final SerializableFinder user = find.byValueKey('user-Chimple');
       await driver.tap(user);
-      await new Future<Duration>.delayed(const Duration(seconds: 2));
-      final SerializableFinder game = find.text('Game');
-      await driver.tap(game);
+
       completer.complete();
       await completer.future;
     }, timeout: const Timeout(const Duration(minutes: 1)));
 
+    test('Game',() async{
+
+      final Completer<Null> completer = new Completer<Null>();
+      await new Future<Duration>.delayed(const Duration(seconds: 2));
+
+      final SerializableFinder game = find.text('Game');
+      await driver.tap(game);
+
+      completer.complete();
+      await completer.future;
+    }, timeout: const Timeout(const Duration(minutes: 1)));
 
     test('scrolling', () async {
       final Completer<Null> completer = new Completer<Null>();
