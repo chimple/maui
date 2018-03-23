0477a52baf3b731fe3bda06a9379aeec84c390c1
ramaranjandey <36834875+ramaranjandey@users.noreply.github.com>
2018-03-23 16:31:34 +0530

    updated code for UI with scrollable buttons

--- lib/games/casino.dart
+++ lib/games/casino.dart
@@ -1,17 +1,46 @@
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
-
-void main()=> runApp(new Casino());
+import 'package:flutter/cupertino.dart';
 
 const List<String> letters = const <String>[
-    'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z'
+];
 
 class Casino extends StatefulWidget {
-    @override
-  _CasinoState createState() => new _CasinoState();
+  Function onScore;
+  Function onProgress;
+  Function onEnd;
+  int iteration;
+
+  Casino({key, this.onScore, this.onProgress, this.onEnd, this.iteration})
+      : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => new _CasinoState();
 }
 
 class _CasinoState extends State<Casino> {
@@ -20,86 +49,73 @@ class _CasinoState extends State<Casino> {
   Widget _buildScrollButton() {
     final FixedExtentScrollController scrollController =
         new FixedExtentScrollController(initialItem: _selectedItemIndex);
-      
+
     return new Container(
       height: 100.0,
       width: 50.0,
       child: new DefaultTextStyle(
-             style: const TextStyle(
-                   color: Colors.red,
-                   fontSize: 30.0,
-                   fontWeight:FontWeight.w900
-             ),
-             child: new SafeArea(
-                    child: new CupertinoPicker(
-                           scrollController: scrollController,
-                           itemExtent: 30.0,
-                           backgroundColor: CupertinoColors.white,
-                           onSelectedItemChanged: (int index) {
-                                                  setState(() {
-                                                  _selectedItemIndex = index;
-                                                  }
-                                                  );
-                                                 },
-                           children: new List<Widget>.generate(letters.length, (int index) {
-                                         return new Center(
-                                                child:new Text(letters[index]),
-                                                );
-                                    }),
-                           ),
-                       ),
-              ),
-           );
+        style: const TextStyle(
+            color: Colors.red, fontSize: 30.0, fontWeight: FontWeight.w900),
+        child: new SafeArea(
+          child: new CupertinoPicker(
+            scrollController: scrollController,
+            itemExtent: 30.0,
+            backgroundColor: CupertinoColors.white,
+            onSelectedItemChanged: (int index) {
+              setState(() {
+                _selectedItemIndex = index;
+              });
+            },
+            children: new List<Widget>.generate(letters.length, (int index) {
+              return new Center(
+                child: new Text(letters[index]),
+              );
+            }),
+          ),
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-           home: new Scaffold(
-                 appBar: new AppBar(
-                         title: const Text('Casino'),
-                        ),
-                 body: new Container(
-                       color: Colors.blue,
-                       child: new Column(
-                              children: <Widget>[
-                                        new Container(
-                                        height: 100.0,
-                                        width: 200.0,
-                                        color: Colors.pinkAccent,
-                                        child: new Center(
-                                               child: new Text('APPLE',
-                                                      textAlign: TextAlign.center,
-                                                      overflow: TextOverflow.ellipsis,
-                                                      style: new TextStyle(fontWeight: FontWeight.bold,
-                                                             fontSize:50.0,
-                                                             letterSpacing:5.0,
-                                                             color: Colors.white
-                                                             ),
-                                                      )
-                                              )
-                                        ),
-                                      
-                                       new Expanded(
-                                          child: new Container(
-                                                 child: new Row(
-                                                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                                                        children: <Widget>[
-                                                                   _buildScrollButton(),
-                                                                   _buildScrollButton(),
-                                                                   _buildScrollButton(),
-                                                                   _buildScrollButton(),
-                                                                   _buildScrollButton(),
-                                                                     
-                         
-                                                        ],
-                                                      ),
-                                                ),
-                                      ),
-                                  ],
-                              ),
-                          ),
-                      ),
-                  );
-       }
+    return new Expanded(
+      child: new Container(
+        color: Colors.blue,
+        child: new Column(
+          children: <Widget>[
+            new Container(
+                height: 100.0,
+                width: 200.0,
+                color: Colors.pinkAccent,
+                child: new Center(
+                    child: new Text(
+                  'APPLE',
+                  textAlign: TextAlign.center,
+                  overflow: TextOverflow.ellipsis,
+                  style: new TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 50.0,
+                      letterSpacing: 5.0,
+                      color: Colors.white),
+                ))),
+            new Expanded(
+              child: new Container(
+                child: new Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    _buildScrollButton(),
+                    _buildScrollButton(),
+                    _buildScrollButton(),
+                    _buildScrollButton(),
+                    _buildScrollButton(),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
