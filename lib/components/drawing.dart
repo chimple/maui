a7c3782beebbed5fea175f24edf6e7dd1ffa8a3d
Manukumar S B <manu@sutara.org>
2018-03-23 11:40:52 +0530

    drawing updated

--- lib/components/drawing.dart
+++ lib/components/drawing.dart
@@ -1,101 +1,12 @@
-import 'dart:async';
-import 'dart:math';
 import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
 
-class Drawing extends StatefulWidget {
-  Function onScore;
-  Function onProgress;
-  Function onEnd;
-  int iteration;
-
-  Drawing({key, this.onScore, this.onProgress, this.onEnd, this.iteration})
-      : super(key: key);
-
-  @override
-  State<StatefulWidget> createState() => new DrawScreen();
-}
-
-class DrawScreen extends State<Drawing> {
-  DrawScreen({this.points, this.currentPainter, this.getPng});
-
-  List<Offset> points;
-  final VoidCallback currentPainter;
-  final getPng;
-
-  @override
-  Widget build(BuildContext context) {
-    var assetsImage = new AssetImage('assets/apple.png');
-    var image = new Image(image: assetsImage, width: 150.0, height: 150.0);
-
-    return new Container(
-        child: new Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-          new Column(children: <Widget>[
-            image,
-            new Text("APPLE",
-                style:
-                    new TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold)),
-            new Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  new RaisedButton(
-                    child: new Text("Clear"),
-                    color: Colors.blue,
-                    onPressed: onClear,
-                  ),
-                  new RaisedButton(
-                      child: new Text("Undo"),
-                      color: Colors.blue,
-                      onPressed: null),
-                  new RaisedButton(
-                    child: new Text("Send"),
-                    color: Colors.blue,
-                    onPressed: getPngImage,
-                  ),
-                ]),
-          ]),
-          new Column(
-//                      child:  new Container(
-//                        color: Colors.red,
-
-//                          height: 200.0,
-//                          width: 200.0,
-            children: <Widget>[
-              new FittedBox(
-                fit: BoxFit.fill,
-                // otherwise the logo will be tiny
-                child: new MyHomePage(),
-              ),
-            ],
-          ),
-//                    ),
-        ]));
-  }
-
-  onClear() {
-    if (mounted) {
-      setState(() {
-//        widget.points = [];
-      });
-      print("btnnnnn");
-      print(points);
-    }
-  }
-
-  getPngImage() {
-    print("savinggggg");
-//    return currentPainter.getPng();
-  }
-}
-
-class MyHomePage extends StatefulWidget {
+class MyDrawingPage extends StatefulWidget {
   @override
   State createState() => new MyHomePageState();
 }
 
-class MyHomePageState extends State<MyHomePage> {
+class MyHomePageState extends State<MyDrawingPage> {
   List<Offset> _points = [];
   DrawPainting currentPainter;
   @override
@@ -105,7 +16,19 @@ class MyHomePageState extends State<MyHomePage> {
     return new Container(
 //      padding: new EdgeInsets.all(20.0),
       height: 350.0, width: 400.0,
-      color: Colors.yellow,
+//      color: Colors.yellow,
+      margin: new EdgeInsets.all(5.0),
+      decoration: new BoxDecoration(
+        color: const Color(0xFFF1F8E9),
+        boxShadow: [new BoxShadow(
+          color: Colors.green,
+          blurRadius: 5.0,
+        ),],
+        border: new Border.all(
+          color: Colors.black,
+          width: 2.0,
+        ),
+      ),
       child: new ConstrainedBox(
         constraints: const BoxConstraints.expand(),
         child: new GestureDetector(
@@ -114,7 +37,7 @@ class MyHomePageState extends State<MyHomePage> {
               RenderBox referenceBox = context.findRenderObject();
 
               Offset localPosition =
-                  referenceBox.globalToLocal(details.globalPosition);
+              referenceBox.globalToLocal(details.globalPosition);
               _points = new List.from(_points)..add(localPosition);
               print("manuu");
               print(_points);
@@ -173,7 +96,7 @@ class DrawPainting extends CustomPainter {
     Paint paint = new Paint()
       ..color = Colors.black
       ..strokeCap = StrokeCap.round
-      ..strokeWidth = 10.0;
+      ..strokeWidth = 6.0;
 
     var path = new Path();
 
@@ -186,7 +109,7 @@ class DrawPainting extends CustomPainter {
           (points[i + 1].dx >= 0 &&
               points[i + 1].dy >= 0 &&
               points[i + 1].dx < size.width &&
-              points[i].dy < size.height))
+              points[i + 1].dy < size.height))
         canvas.drawLine(points[i], points[i + 1], paint);
     }
   }
---
+++ lib/games/drawing_game.dart
@@ -0,0 +1,89 @@
+import 'dart:async';
+import 'dart:math';
+import 'dart:ui' as ui;
+import 'package:flutter/material.dart';
+import '../components/drawing.dart';
+
+class Drawing extends StatefulWidget {
+  Function onScore;
+  Function onProgress;
+  Function onEnd;
+  int iteration;
+
+  Drawing({key, this.onScore, this.onProgress, this.onEnd, this.iteration})
+      : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => new DrawScreen();
+}
+
+class DrawScreen extends State<Drawing> {
+  DrawScreen({this.points, this.currentPainter, this.getPng});
+
+  List<Offset> points;
+  final VoidCallback currentPainter;
+  final getPng;
+
+  @override
+  Widget build(BuildContext context) {
+    var assetsImage = new AssetImage('assets/apple.png');
+    var image = new Image(image: assetsImage, width: 150.0, height: 150.0);
+
+    return new Container(
+        child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+          new Column(children: <Widget>[
+            image,
+            new Text("APPLE",
+                style:
+                    new TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold)),
+            new Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  new RaisedButton(
+                    child: new Text("Clear"),
+                    color: Colors.blue,
+                    onPressed: onClear,
+                  ),
+                  new RaisedButton(
+                      child: new Text("Undo"),
+                      color: Colors.blue,
+                      onPressed: null),
+                  new RaisedButton(
+                    child: new Text("Send"),
+                    color: Colors.blue,
+                    onPressed: getPngImage,
+                  ),
+                ]),
+          ]),
+          new Column(
+//                      child:  new Container(
+//                        color: Colors.red,
+
+//                          height: 200.0,
+//                          width: 200.0,
+            children: <Widget>[
+              new FittedBox(
+                fit: BoxFit.fill,
+                // otherwise the logo will be tiny
+                child: new MyDrawingPage(),
+              ),
+            ],
+          ),
+//                    ),
+        ]));
+  }
+
+  onClear() {
+
+      print("btnnnnn");
+
+  }
+
+  getPngImage() {
+    print("savinggggg");
+//    return currentPainter.getPng();
+  }
+}
+
