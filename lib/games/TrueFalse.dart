ae6881563fd330010084850c3cd8db34aa55e997
Karan <karan@sutara.org>
2018-03-27 10:11:58 +0530

    Changed the layout to use containers and columns intead of stack

--- lib/games/TrueFalse.dart
+++ lib/games/TrueFalse.dart
@@ -75,53 +75,62 @@ class QuizPageState extends State<QuizPage> {
 
   @override
   Widget build(BuildContext context) {
-    return new Stack(
-      fit: StackFit.loose,
-      children: <Widget>[
-        new Column(
-        mainAxisAlignment: MainAxisAlignment.center,       
-           children: <Widget>[
-             new Column(
-               mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget>[new Column( 
-               mainAxisAlignment: MainAxisAlignment.end,
-             children: <Widget>[ 
-               new QuestionText(questionText, questionNumber),
-              new Padding(
-              padding: new EdgeInsets.all(85.0),
-            ),
-             ]
-           ),
-              ],
-             ),
-            new Row(
-              mainAxisAlignment: MainAxisAlignment.end,
+    return new Container(
+        child: new Column(
+          children: <Widget>[
+            new Column(
               children: <Widget>[
                 new Padding(
-                    padding: new EdgeInsets.all(15.0),
-                  ),
-                  new AnswerButton(true, () => handleAnswer(true)), //true button
-                  new Padding(
-                    padding: new EdgeInsets.all(15.0),
-                  ),
-                  new AnswerButton(false, () => handleAnswer(false)), 
-                  new Padding(
-                    padding: new EdgeInsets.all(15.0),
-                  ),// false button
-                ],
+                          padding: new EdgeInsets.all(85.0),
+                        ),
+                new Container(
+                    height: 100.0,
+                    width: 200.0,
+                    child: new Center(
+                        child: new QuestionText(questionText, questionNumber),
+                    ),
+                ),
+
+                new Padding(
+                          padding: new EdgeInsets.all(85.0),
+                        ),
+
+                new Container(
+                    child: new Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        new Padding(
+                          padding: new EdgeInsets.all(15.0),
+                        ),
+
+                      new AnswerButton(true, () => handleAnswer(true)), //true button
+
+                      new Padding(
+                          padding: new EdgeInsets.all(15.0),
+                        ),
+                      
+                      new AnswerButton(false, () => handleAnswer(false)), //false button
+
+                      new Padding(
+                          padding: new EdgeInsets.all(15.0),
+                        ),
+                    ],
+                    ),
+                  )
+              ]
             ),
-            new Padding(
-              padding: new EdgeInsets.only(bottom: 100.0),
-            ),
-          ],
-        ),
-        overlayShouldBeVisible == true ? new CorrectWrongOverlay(
-            isCorrect,
-                () {
-              if (quiz.length == questionNumber) {
-                 widget.onEnd();
-                return;
-              }
+
+            overlayShouldBeVisible == true ? new Expanded(
+              child: new Container(
+                child: new Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[new CorrectWrongOverlay(
+                  isCorrect,
+                  () {
+                  if (quiz.length == questionNumber) {
+                  widget.onEnd();
+                  return;
+                  }
               currentQuestion = quiz.nextQuestion;
               this.setState(() {
                 overlayShouldBeVisible = false;
@@ -129,8 +138,11 @@ class QuizPageState extends State<QuizPage> {
                 questionNumber = quiz.questionNumber;
               });
             }
-        ) : new Container()
-      ],
+        )]))) : new Container()
+
+          ],
+        ),
+            
     );
   }
     
