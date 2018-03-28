bb2c324bf9cec3b7ceccfbb81a91c6dcc60825e5
Anand Bharadwaj <anand@zoolore.org>
2018-03-28 11:57:27 +0530

    new file
    Seperate file added for true_or_false game

--- test_driver/user_list_test.dart
+++ test_driver/user_list_test.dart
@@ -41,36 +41,5 @@ void main() {
       await completer.future;
     }, timeout: const Timeout(const Duration(minutes: 1)));
 
-    test('scrolling', () async {
-      final Completer<Null> completer = new Completer<Null>();
-      await new Future<Duration>.delayed(const Duration(seconds: 2));
-      bool scroll = true;
-      final SerializableFinder menuItem = find.byValueKey('true_or_false');
-
-      driver.waitFor(menuItem).then<Null>((Null value) async {
-        scroll = false;
-        await new Future<Duration>.delayed(const Duration(seconds: 1));
-        await driver.tap(menuItem);
-        await new Future<Duration>.delayed(const Duration(seconds: 1));
-        completer.complete();
-      });
-      final SerializableFinder gs = find.byValueKey('Game_page');
-      while (scroll) {
-        await driver.scroll(gs, 0.0, -500.0, const Duration(milliseconds: 500));
-        await new Future<Null>.delayed(kWaitBetweenActions);
-      }
-      await completer.future;
-    }, timeout: const Timeout(const Duration(minutes: 1)));
-
-    test('playing the game', () async {
-      final Completer<Null> completer = new Completer<Null>();
-      String s = 'Upper Case Letter to Lower Case Letter';
-      final SerializableFinder option = find.text(s);
-      await driver.tap(option);
-      final SerializableFinder mode = find.byValueKey('single');
-      await driver.tap(mode);
-      completer.complete();
-      await completer.future;
-    }, timeout: const Timeout(const Duration(minutes: 1)));
   });
 }
