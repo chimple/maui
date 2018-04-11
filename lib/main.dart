2e283508b83820cbadb251b7ef0f2086890f7699
sutara-aayush <35102016+sutara-aayush@users.noreply.github.com>
2018-04-11 17:51:31 +0530

    Modified main.dart code corrected.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:maui/app.dart';
 import 'package:maui/state/app_state_container.dart';
-import 'package:flutter/rendering.dart';
 
 void main() {
-    debugPaintSizeEnabled = true;
     runApp(new AppStateContainerController(child: new MauiApp(),));
 }
