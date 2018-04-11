e015a43fe49c31dec93bfb3c3b56097ddd2a4f85
sutara-aayush <35102016+sutara-aayush@users.noreply.github.com>
2018-04-11 17:19:14 +0530

    New image and json data added. Game Working but hard coded.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:maui/app.dart';
 import 'package:maui/state/app_state_container.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
+    debugPaintSizeEnabled = true;
     runApp(new AppStateContainerController(child: new MauiApp(),));
 }
