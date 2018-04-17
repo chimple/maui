ff888210c9236ae499514aff740ed60072c91972
Anand Bharadwaj <anand@zoolore.org>
2018-04-17 11:27:43 +0530

    updated identif.dart

--- test_driver/identify_test.dart
+++ test_driver/identify_test.dart
@@ -13,23 +13,31 @@ void main() {
       commonSignIn(driver);
       commonGoToGames(driver);
       commonScrolling(driver, 'identify');
-      await new Future<Duration>.delayed(const Duration(seconds: 1) );
+      await new Future<Duration>.delayed(const Duration(seconds: 4) );
     });
 
+   test('playingGame', () async {
+    await new Future<Duration>.delayed(const Duration(seconds: 1));
+    final Completer<Null> completer = new Completer<Null>();
+    await new Future<Duration>.delayed(const Duration(seconds: 1));
+    final SerializableFinder option = find.text("Todo Placeholder");
+     await driver.tap(option);
+     await new Future<Duration>.delayed(const Duration(seconds: 1));
+     final SerializableFinder mode = find.byValueKey('single');
+     await driver.tap(mode);
+     final SerializableFinder element1 = find.text('square');
+    final SerializableFinder element2 = find.text('circle');
+    final SerializableFinder element3 = find.text('triangle');
+    final SerializableFinder element4 = find.text('hexagon');
 
-    test('playingGame', () async {
-      await new Future<Duration>.delayed(const Duration(seconds: 1));
-      final Completer<Null> completer = new Completer<Null>();
-      await new Future<Duration>.delayed(const Duration(seconds: 1));
-      final SerializableFinder option = find.text('Todo Placeholder');
-      await driver.tap(option);
-      await new Future<Duration>.delayed(const Duration(seconds: 2));
-      final SerializableFinder mode = find.byValueKey('single');
-      await driver.tap(mode);
+    await driver.scroll(element1, 0.0, -500.0, const Duration(milliseconds:400 ));
+    await driver.scroll(element2, -300.0, -500.0, const Duration(milliseconds:400 ));
+    await driver.scroll(element3, 0.0, -250.0, const Duration(milliseconds:400 ));
+    await driver.scroll(element4, 0.0, -500.0, const Duration(milliseconds:400 ));
 
+     completer.complete();
+     await completer.future;
+   }, timeout: const Timeout(const Duration(minutes: 1)));
 
-
-    }, timeout: const Timeout(const Duration(minutes: 1)));
-
-  });
+ });
 }
