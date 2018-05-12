88e07dea0c7b3b6d86f1b55f7a8708dd3e30136f
SATISH <36834824+satish754ss@users.noreply.github.com>
2018-05-12 13:19:23 +0530

    Updated Codes

--- lib/games/match_the_following.dart
+++ lib/games/match_the_following.dart
@@ -5,7 +5,6 @@ import 'package:flutter/material.dart';
 import 'package:maui/components/responsive_grid_view.dart';
 import 'package:maui/games/single_game.dart';
 import 'package:maui/repos/game_data.dart';
-//import 'package:maui/components/shaker.dart';
 import 'package:maui/components/unit_button.dart';
 import 'package:maui/state/app_state_container.dart';
 import 'package:maui/state/app_state.dart';
@@ -59,16 +58,11 @@ class _MatchTheFollowingState extends State<MatchTheFollowing>
   final int score = 2;
   int indexText1, indexText2, indexLeftButton;
   int _oldIndexforLeftButton = 0,
-      _nextTask,
+      _numButtons,
       leftIsTapped = 0,
       leftSideTextIndex = 0;
   bool _isLoading = true;
   int indexL, flag = 0, flag1 = 0, correct = 0, _wrongAttem = 0;
-  List<String> image = [
-    'assets/back.jpg',
-    'assets/back1.jpg',
-    'assets/background.jpg'
-  ];
   List<int> _shake = [];
   String loading = 'Loading..';
   int c1 = 0;
@@ -92,7 +86,7 @@ class _MatchTheFollowingState extends State<MatchTheFollowing>
         ? _rightSideLetters.fold(maxChars,
             (prev, element) => element.length > prev ? element.length : prev)
         : 1);
-
+        print("MaxChar value:: $maxChars");
     return new LayoutBuilder(builder: (context, constraints) {
       final hPadding = pow(constraints.maxWidth / 150.0, 2);
       final vPadding = pow(constraints.maxHeight / 150.0, 2);
@@ -100,7 +94,7 @@ class _MatchTheFollowingState extends State<MatchTheFollowing>
       double maxWidth =
           (constraints.maxWidth - hPadding * 2) / 2 - middle_spacing;
       double maxHeight =
-          (constraints.maxHeight - vPadding * 2) / _allLetters.length;
+          (constraints.maxHeight - vPadding * 2) / _numButtons;
 
       final buttonPadding = sqrt(min(maxWidth, maxHeight) / 5);
 
@@ -118,32 +112,11 @@ class _MatchTheFollowingState extends State<MatchTheFollowing>
               new Expanded(
                 child: _buildLeftSide(context, buttonPadding),
               ),
-              new Padding(padding: new EdgeInsets.all(middle_spacing)),
+              new Padding(padding: new EdgeInsets.symmetric(horizontal: middle_spacing)),
               new Expanded(child: _buildRightSide(context, buttonPadding)),
             ],
           ));
     });
-    // return new Stack(
-    //   fit: StackFit.expand,
-    //   children: <Widget>[
-    //         new Container(
-    //   //        child: new Image(
-    //   //          fit: BoxFit.fill,
-    //   //      image: new AssetImage(image[0])
-    //   // ),
-    //   ),
-    //       new Row(
-    //       mainAxisAlignment: MainAxisAlignment.center,
-    //       children: <Widget>[
-    //         new Expanded(
-    //           child: _buildLeftSide(context),
-    //         ),
-    //        new Padding(padding: new EdgeInsets.all(60.0)),
-    //         new Expanded(child: _buildRightSide(context)),
-    //       ],
-    //     )
-    //   ],
-    // );
   }
 
   int _constant, _constant1;
@@ -152,17 +125,17 @@ class _MatchTheFollowingState extends State<MatchTheFollowing>
     print("initState called::");
     if (widget.gameConfig.level < 4) {
       print("level <4");
-      _nextTask = 2;
+      _numButtons = 2;
       _constant = 0;
       _constant1 = 0;
     } else if (widget.gameConfig.level < 6) {
       print("level <8");
-      _nextTask = 4;
+      _numButtons = 4;
       _constant = 0;
       _constant1 = 1;
     } else {
       print("level <10");
-      _nextTask = 6;
+      _numButtons = 6;
       _constant = 1;
       _constant1 = 2;
     }
@@ -196,22 +169,22 @@ class _MatchTheFollowingState extends State<MatchTheFollowing>
 
   void _initBoard() async {
     setState(() => _isLoading = true);
-    print('initBoard $_nextTask');
+    print('initBoard $_numButtons');
     _allLetters =
-        await fetchPairData(widget.gameConfig.gameCategoryId, _nextTask);
+        await fetchPairData(widget.gameConfig.gameCategoryId, _numButtons);
     _allLetters.forEach((k, v) {
       _leftSideletters.add(k);
       _rightSideLetters.add(v);
     });
     _shuffledLetters.addAll(
-        _leftSideletters.take(_nextTask).toList(growable: false)..shuffle());
+        _leftSideletters.take(_numButtons).toList(growable: false)..shuffle());
     _shuffledLetters1.addAll(
-        _rightSideLetters.take(_nextTask).toList(growable: false)..shuffle());
-    _lettersLeft = _shuffledLetters.sublist(0, _nextTask);
-    _lettersRight = _shuffledLetters1.sublist(0, _nextTask);
+        _rightSideLetters.take(_numButtons).toList(growable: false)..shuffle());
+    _lettersLeft = _shuffledLetters.sublist(0, _numButtons);
+    _lettersRight = _shuffledLetters1.sublist(0, _numButtons);
     _statusColorChange =
         _shuffledLetters.map((a) => Status.Disable).toList(growable: false);
-    for (int i = 0; i <= _nextTask * 2 - 1; i++) {
+    for (int i = 0; i <= _numButtons * 2 - 1; i++) {
       _shake.add(0);
     }
     setState(() => _isLoading = false);
@@ -221,7 +194,7 @@ class _MatchTheFollowingState extends State<MatchTheFollowing>
   Widget _buildLeftSide(BuildContext context, double buttonPadding) {
     int j = 0;
     return new ResponsiveGridView(
-      rows: _nextTask,
+      rows: _numButtons,
       cols: 1,
       children: _lettersLeft
           .map((e) => Padding(
@@ -261,9 +234,9 @@ class _MatchTheFollowingState extends State<MatchTheFollowing>
   }
 
   Widget _buildRightSide(BuildContext context, double buttonPadding) {
-    int j = _nextTask;
+    int j = _numButtons;
     return new ResponsiveGridView(
-      rows: _nextTask,
+      rows: _numButtons,
       cols: 1,
       padding: 4.0,
       maxAspectRatio: 1.3,
@@ -292,18 +265,12 @@ class _MatchTheFollowingState extends State<MatchTheFollowing>
     if (leftIsTapped == 1) if (leftSideTextIndex ==
             _rightSideLetters.indexOf(_rightSideText) ||
         identical(_rightSideText, _leftSideText)) {
-      // if (identical(question, answer)) {
-
       setState(() {
-        //  _lettersLeft[indexText1] = null;
-        // _lettersRight[indexText2] = null;
-        //_statusShake[indexRightbutton] = Status.Dump;
-        //_statusColorChange[indexLeftButton] = Status.Disable;
         _shake[indexRightbutton] = 1;
       });
       correct++;
       widget.onScore(1);
-      widget.onProgress(correct / _nextTask);
+      widget.onProgress(correct / _numButtons);
       leftIsTapped = 0;
     } else {
       leftSideTextIndex = -1;
@@ -312,7 +279,7 @@ class _MatchTheFollowingState extends State<MatchTheFollowing>
         try {
           setState(() {
             _shake[indexRightbutton] = 1;
-            //  _statusShake[indexRightbutton] = Status.Shake;
+           
             _statusColorChange[indexLeftButton] = Status.Shake;
             _shake[indexRightbutton] = 2;
             flag1 = 1;
@@ -321,7 +288,7 @@ class _MatchTheFollowingState extends State<MatchTheFollowing>
         try {
           new Future.delayed(const Duration(milliseconds: 700), () {
             setState(() {
-              //_statusShake[indexRightbutton] = Status.Stopped;
+              
               _statusColorChange[indexLeftButton] = Status.Disable;
               _shake[indexRightbutton] = 0;
             });
@@ -332,24 +299,21 @@ class _MatchTheFollowingState extends State<MatchTheFollowing>
       }
     }
     if (_wrongAttem >= correct - _constant &&
-        _wrongAttem == _nextTask - _constant1) {
+        _wrongAttem == _numButtons - _constant1) {
       _wrongAttem = 0;
       widget.onScore(-correct);
       correct = 0;
       new Future.delayed(const Duration(milliseconds: 700), () {
         widget.onEnd();
-        // _initBoard();
+        
       });
     }
 
-    if (correct == _nextTask) {
-      //print('Game Over::');
-      //  print("score::$correct-$_wrongAttem}");
+    if (correct == _numButtons) {
+      
       _wrongAttem = 0;
       correct = 0;
       widget.onScore(-_wrongAttem);
-
-      //setState(() {});
       new Future.delayed(const Duration(milliseconds: 1000), () {
         _leftSideletters.clear();
         _rightSideLetters.clear();
@@ -364,7 +328,7 @@ class _MatchTheFollowingState extends State<MatchTheFollowing>
       });
     }
     print("Correct ::$correct ");
-    print("Total task:: $_nextTask");
+    print("Total task:: $_numButtons");
   }
 }
 
@@ -431,24 +395,9 @@ class _MyButtonState extends State<MyButton> with TickerProviderStateMixin {
     ).animate(controllerShake);
     controller.addStatusListener((state) {
       if (state == AnimationStatus.completed) {
-        // if (widget.text == null) {
         setState(() => _displayText = widget.text);
-        //}
       }
     });
-    // borderRadius = new BorderRadiusTween(
-    //   begin: new BorderRadius.circular(4.0),
-    //   end: new BorderRadius.circular(75.0),
-    // ).animate(
-    //   new CurvedAnimation(
-    //     parent: radiusCntroller,
-    //     curve: new Interval(
-    //       0.375,
-    //       0.500,
-    //       curve: Curves.ease,
-    //     ),
-    //   ),
-    // );
     controller.forward();
     shake();
   }
@@ -461,7 +410,6 @@ class _MyButtonState extends State<MyButton> with TickerProviderStateMixin {
         if (status == AnimationStatus.completed) {
           controllerPress.reverse();
         } else if (status == AnimationStatus.dismissed) {
-          //controllerPress.forward();
         }
       });
     }
@@ -489,25 +437,6 @@ class _MyButtonState extends State<MyButton> with TickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-    final Color GRADIENT_TOP = const Color(0xFFF5F5F5);
-    final Color GRADIENT_BOTTOM = const Color(0xFFE8E8E8);
-    const double radius = 8.0;
-    Size media = MediaQuery.of(context).size;
-    double _w = media.height;
-    _w = _w / 20;
-    int _color = 0xFFed4a79;
-    int changeColor = 0xFFed4a79;
-    if (widget.shake == 2) {
-      _color = 0xFFff0000; // red
-    }
-    if (widget.status == Status.Enable) {
-      changeColor = 0xFFFA8072;
-      //widget.disableColor=changeColor;
-    }
-    if (widget.shake == 1) {
-      changeColor = 0xFF00FF00;
-    }
-
     return new Shake(
         animation: widget.status == Status.Shake || widget.shake == 2
             ? animationShake
@@ -520,30 +449,8 @@ class _MyButtonState extends State<MyButton> with TickerProviderStateMixin {
                 ? () => widget.onPress()
                 : null,
             text: _displayText,
-            unitMode: UnitMode.image,
-            // disableColor: widget.status == Status.Enable || widget.shake == 1
-            //     ? new Color(changeColor)
-            //     : new Color(_color),
+            unitMode: UnitMode.text,
           ),
-          // child: new RaisedButton(
-          //     //splashColor: new Color(0xFFed4a79),
-          //     //color: new Color(_color),
-          //     disabledColor: widget.status == Status.Enable || widget.shake == 1
-          //         ? new Color(changeColor)
-          //         : new Color(_color), // red color
-          //     elevation: 8.0,
-          //     onPressed: (widget.status == Status.Disable || widget.shake == 0)
-          //         ? () => widget.onPress()
-          //         : null,
-          //     shape: new RoundedRectangleBorder(
-          //         borderRadius: widget.shake == 1
-          //             ? const BorderRadius.all(const Radius.circular(16.0))
-          //             : const BorderRadius.all(const Radius.circular(radius))),
-          //     child: new Text(_displayText,
-          //         style: new TextStyle(
-          //             color: Colors.white,
-          //             fontSize: _w,
-          //             fontStyle: FontStyle.italic))),
         ));
   }
 }
@@ -561,13 +468,6 @@ class Shake extends AnimatedWidget {
   double get translate {
     final double t = animation.value;
     const double shakeDelta = 2.0;
-    // if (t <= 0.25)
-    //   return -t * shakeDelta;
-    // else if (t < 0.75)
-    //   return - (t - 0.5) * shakeDelta;
-    // else
-    //   return -(1.0 - t) * 4.0 * shakeDelta;
-    // return -t*1.2;
     if (t <= 1) {
       return pi * t / 45;
     } else {
