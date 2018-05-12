d36a0f121149660c6e28f9f56eaf57e65758cad7
Manukumar S B <manu@sutara.org>
2018-05-12 22:40:11 +0530

    dice_game gif animation

--- lib/games/dice_game.dart
+++ lib/games/dice_game.dart
@@ -1,9 +1,11 @@
+import 'dart:async';
 import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:maui/games/single_game.dart';
 import 'package:maui/repos/game_data.dart';
 import 'package:maui/components/responsive_grid_view.dart';
 import 'package:maui/components/Shaker.dart';
+import 'package:maui/components/unit_button.dart';
 
 class Dice extends StatefulWidget {
   Function onScore;
@@ -28,6 +30,8 @@ class Dice extends StatefulWidget {
 }
 enum Stext1 {Active, Visible}
 enum Stext2 { Right, InActive }
+enum Statuses {right,wrong}
+
 class DiceGameState extends State<Dice> {
   var flag1 = 0;
   var correct = 0;
@@ -64,11 +68,12 @@ class DiceGameState extends State<Dice> {
   List<String> _rightwords = [];
   List<Stext1> _status1=[];
   List<Stext2> _status2=[];
+  List<Statuses> _statusList ;
 
   List<String> diceData = ['1', '2', '3', '4', '5', '6'];
   List _sortletters = [];
   bool _isLoading = true;
-  List<int> X = [];
+  List<int> dice_tries = [];
   int code, dindex, dcode;
   List<String> arr = new List<String>();
   String _counter = "";
@@ -86,14 +91,14 @@ class DiceGameState extends State<Dice> {
   void initState() {
     super.initState();
     _initBoard();
+    _statusList=[];
   }
 
   void _initBoard() async {
     setState(() => _isLoading = true);
     _status1 = data.map((a)=>Stext1.Active).toList(growable: false);
     _status2 = data.map((a)=>Stext2.InActive).toList(growable: false);
-    _status1 = data1.map((a)=>Stext1.Active).toList(growable: false);
-    _status2 = data1.map((a)=>Stext2.InActive).toList(growable: false);
+
 
   }
 
@@ -108,11 +113,11 @@ class DiceGameState extends State<Dice> {
         onPress: () {
           count1=count1+1;
             setState(() {
-              var Z = int.parse(text);
-              for (var i = 0; i < X.length; i++) {
+              var btnVal = int.parse(text);
+              for (var i = 0; i < dice_tries.length; i++) {
 
-                sum = sum + X[i];
-                sub = X[i] - (sub);
+                sum = sum + dice_tries[i];
+                sub = dice_tries[i] - (sub);
                 print({"manu sum": sum});
                 print({"manu sub": sub});
               }
@@ -123,29 +128,39 @@ class DiceGameState extends State<Dice> {
                 if(e==Stext1.Visible)
                   {
                     flag2=1;
-                  }
+                  } 
               });
-//              _status2.forEach((e){
-//                if(e==Stext2.InActive)
-//                {
-//                  flag2=0;
-//                }
-//              });
+            //  _status2.forEach((e){
+            //    if(e==Stext2.InActive)
+            //    {
+            //      flag2=0;
+            //    }
+            //  });
 
               if(flag2==0) {
                 if (status == Stext1.Active) {
-                  if (Z == sum) {
+                  if (btnVal == sum) {
                     count = 0;
-                    X.removeRange(0, X.length);
+                    dice_tries.removeRange(0, dice_tries.length);
                     _counter = " ";
                     _counter1 = " ";
                     _counter2 = " ";
                     _counter3 = " ";
                     sum = 0;
                     sub = 0;
-                    setState(() {
-                      _status1[index] = Stext1.Visible;
-                    });
+                    // new Future.delayed(const Duration(milliseconds: 200), () {
+                    //   setState(() {
+                    //     //   _statusList.removeAt(index);
+                    //     _status1[index] = Stext1.Visible;
+                    //     print({"index is": index});
+
+
+                    //   });
+                    // });
+
+                   setState(() {
+                     _status1[index] = Stext1.Visible;
+                   });
                     if (flag == 0) {
                       flag = 1;
                     }
@@ -154,10 +169,10 @@ class DiceGameState extends State<Dice> {
                     }
                   }
 
-                  else if (sub == Z) {
+                  else if (sub == btnVal) {
                     flag = 1;
                     count = 0;
-                    X.removeRange(0, X.length);
+                    dice_tries.removeRange(0, dice_tries.length);
                     _counter = " ";
                     _counter1 = " ";
                     _counter2 = " ";
@@ -179,9 +194,9 @@ class DiceGameState extends State<Dice> {
               else{
                 print("mannu is data is");
                 if (status == Stext1.Active) {
-                  if (Z == sum) {
+                  if (btnVal == sum) {
                     count = 0;
-                    X.removeRange(0, X.length);
+                    dice_tries.removeRange(0, dice_tries.length);
                     _counter = " ";
                     _counter1 = " ";
                     _counter2 = " ";
@@ -203,10 +218,10 @@ class DiceGameState extends State<Dice> {
                     }
                   }
 
-                  else if (sub == Z) {
+                  else if (sub == btnVal) {
                     flag = 1;
                     count = 0;
-                    X.removeRange(0, X.length);
+                    dice_tries.removeRange(0, dice_tries.length);
                     _counter = " ";
                     _counter1 = " ";
                     _counter2 = " ";
@@ -244,8 +259,8 @@ class DiceGameState extends State<Dice> {
           final _random = new Random();
           var dElement = diceData[_random.nextInt(diceData.length)];
 
-          var f = int.parse(dElement);
-          X.add(f);
+          var randval = int.parse(dElement);
+          dice_tries.add(randval);
           _counter1 = dElement;
           _counter = "$_counter" + "$dElement" + ",";
           print("dice data $_counter ");
@@ -257,8 +272,8 @@ class DiceGameState extends State<Dice> {
           final _random = new Random();
           var dElement = diceData[_random.nextInt(diceData.length)];
 
-          var f = int.parse(dElement);
-          X.add(f);
+          var randval = int.parse(dElement);
+          dice_tries.add(randval);
           _counter1 = dElement;
           _counter3 = "$_counter3" + "$dElement" + ",";
           print("dice data $_counter3 ");
@@ -300,26 +315,26 @@ class DiceGameState extends State<Dice> {
                        style: new TextStyle(
                            color: Colors.black, fontSize: 25.0)))
            ),
-       new GestureDetector(
+       new InkWell(
           onTap: _randomVal,
            child: new Container(
               height: media.size.height > media.size.width ?constraints.maxHeight*.2 : constraints.maxHeight*.3 ,
                width: media.size.height < media.size.width? constraints.maxWidth*.2 : constraints.maxWidth*.3 ,
-               decoration: new BoxDecoration(
-                 color: const Color(0xFFF1F8E9),
-                 boxShadow: [new BoxShadow(
-                   color: Colors.green,
-                   blurRadius: 5.0,
-                 ),
-                 ],
-                 border: new Border.all(
-                   color: Colors.black,
-                   width: 5.0,
-                 ),
-               ),
-              child: new Center(
-              child: new Text('$_counter1',
-              style: new TextStyle(fontSize: 50.0))
+              // decoration: new BoxDecoration(
+              //   color: const Color(0xFFF1F8E9),
+              //   boxShadow: [new BoxShadow(
+              //     color: Colors.green,
+              //     blurRadius: 5.0,
+              //   ),
+              //   ],
+              //   border: new Border.all(
+              //     color: Colors.black,
+              //     width: 5.0,
+              //   ),
+              // ),
+              child: new Image(
+              image:new AssetImage('assets/dice5.gif')
+              //style: new TextStyle(fontSize: 50.0))
             ))),
            new Container(
              height: 50.0, width: 100.0,
@@ -428,10 +443,11 @@ class _MyButtonState extends State<MyButton> with TickerProviderStateMixin {
     return new ScaleTransition(
         scale: animation,
         child:new Container(
-        child: new RaisedButton(
-          onPressed: widget.onPress,
-          child: new Text("$_displayText",
-          style:new TextStyle( fontSize: 20.0),),
+          // color: Colors.white,
+        child: new UnitButton(
+          onPress: widget.onPress,
+          text: widget.text,
+          unitMode: UnitMode.text,
         )));
   }
 }
