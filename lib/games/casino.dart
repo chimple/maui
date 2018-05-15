0cd9e647ac5a194e55c706561ce3b882410b7774
ramaranjandey <36834875+ramaranjandey@users.noreply.github.com>
2018-05-15 15:49:38 +0530

    bug resolved for not reconizing right letters

--- lib/games/casino.dart
+++ lib/games/casino.dart
@@ -106,7 +106,15 @@ class _CasinoState extends State<Casino> {
           "scrolling[random] ${scrollingLetterList[random]}   givenletter ${givenWordList[j]}");
       if (givenWordList[j] == scrollingLetterList[random]) {
         _selectedItemIndex = givenWordList.length - 1;
+        if (scrollingLetterList[random] == 'a' ||
+            scrollingLetterList[random] == 'A') {
+              var temp = scrollingLetterList[givenWordList.length - 1];
+              scrollingLetterList[givenWordList.length - 1] = scrollingLetterList[random];
+              scrollingLetterList[0] == temp;
+            }
+
         print("Hey data shuffled");
+
         print("scrollingLetterList = $scrollingLetterList");
       }
       j++;
