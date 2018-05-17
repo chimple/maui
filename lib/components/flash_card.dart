6f6b02da0046ad2a058614f03bd56c832cd246a3
Anuj-Chimple <anuj@sutara.org>
2018-05-17 23:00:59 +0530

    updated flash card

--- lib/components/flash_card.dart
+++ lib/components/flash_card.dart
@@ -64,20 +64,33 @@ class _FlashCardState extends State<FlashCard> {
                     new IconButton(
                         icon: new Icon(Icons.arrow_left),
                         onPressed: widget.onChecked,
-                        iconSize: constraints.maxHeight * 0.15,
+                        iconSize: constraints.maxHeight * 0.12,
                         color: Colors.white),
                     new Expanded(child
                         : new SizedBox(  height:  constraints.maxHeight > constraints.maxWidth ? constraints.maxHeight * 0.4 : constraints.maxWidth * 0.3,
                         width: constraints.maxHeight > constraints.maxWidth ? constraints.maxWidth * 0.9 : constraints.maxHeight * 0.5,
-                        child: widget.image == null ? new Image.asset ('assets/apple.png') : new Image.asset(widget.image))),
+                        child: widget.image == null ?
+                        new Container(
+                            alignment: const Alignment(0.0, 0.0),
+                            child: new Text(widget.text,  style: new TextStyle(color: Colors.white, fontSize: constraints.maxHeight * 0.11, fontWeight: FontWeight.bold))) :
+                        new Image.asset(widget.image))),
                     new IconButton(
                       icon: new Icon(Icons.arrow_right),
                       onPressed: widget.onChecked,
-                      iconSize: constraints.maxHeight * 0.15,
+                      iconSize: constraints.maxHeight * 0.12,
                       color: Colors.white,
                     )
                   ],
                 ),
+                widget.image == null  ? new Container(
+                    alignment: const Alignment(0.0, 0.0),
+                    margin: new EdgeInsets.all(constraints.maxHeight * 0.04),
+                    decoration: new BoxDecoration(
+                        borderRadius: new BorderRadius.all(
+                            new Radius.circular(constraints.maxHeight * 0.015)),
+                        shape: BoxShape.rectangle),
+                    child: new Text("",
+                        style: new TextStyle(color: Colors.white, fontSize: constraints.maxHeight * 0.1, fontWeight: FontWeight.bold ))) :
                 new Container(
                     height: constraints.maxHeight * 0.2,
                     width: constraints.maxWidth * 0.9,
@@ -89,7 +102,8 @@ class _FlashCardState extends State<FlashCard> {
                             new Radius.circular(constraints.maxHeight * 0.015)),
                         shape: BoxShape.rectangle),
                     child: new Text(_unit?.name ?? widget.text,
-                        style: new TextStyle(color: Colors.white, fontSize: constraints.maxHeight * 0.1, fontWeight: FontWeight.bold ))),
+                        style: new TextStyle(color: Colors.white, fontSize: constraints.maxHeight * 0.1, fontWeight: FontWeight.bold )))
+
               ]));
     });
   }
