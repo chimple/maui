cf427bffc0a9d16b2a4bc4bc0da1e7b5d933a9c1
Manukumar S B <manu@sutara.org>
2018-05-30 03:31:33 +0530

    Updated draw_game and dice_game

--- lib/games/dice_game.dart
+++ lib/games/dice_game.dart
@@ -29,16 +29,17 @@ class Dice extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => new DiceGameState();
 }
-enum Stext1 {Active, Visible}
-enum Stext2 { Right, InActive }
-enum Statuses {right,wrong}
 
-class DiceGameState extends State<Dice> {
+enum Stext1 { Active, Visible }
+enum Stext2 { Right, InActive }
+enum Statuses { right, wrong }
+
+class DiceGameState extends State<Dice> with SingleTickerProviderStateMixin {
   var flag1 = 0;
   var correct = 0;
 
   var keys = 0;
- static List<String> data = [
+  static List<String> data = [
     '1',
     '2',
     '3',
@@ -52,7 +53,8 @@ class DiceGameState extends State<Dice> {
     '11',
     '12'
   ];
-  static List<String> data1 = [
+
+    static List<String> data1 = [
     '1',
     '2',
     '3',
@@ -66,10 +68,11 @@ class DiceGameState extends State<Dice> {
     '11',
     '12'
   ];
-  List<String> _rightwords = [];
-  List<Stext1> _status1=[];
-  List<Stext2> _status2=[];
-  List<Statuses> _statusList ;
+
+    List<String> _rightwords = [];
+  List<Stext1> _status1 = [];
+  List<Stext2> _status2 = [];
+  List<Statuses> _statusList;
 
   List<String> diceData = ['1', '2', '3', '4', '5', '6'];
   List _sortletters = [];
@@ -80,195 +83,137 @@ class DiceGameState extends State<Dice> {
   String _counter = "";
   String _counter1 = "1";
   String _counter2 = "";
-  String _counter3 = "";
- int count=0;
- int count1=0;
- int sum=0;
- int sub=0;
- int flag=0;
- var flag2=0;
+  int count = 0;
+  int count1 = 0;
+  int flag = 0;
+
+  AnimationController animation;
 
   @override
   void initState() {
     super.initState();
     _initBoard();
-    _statusList=[];
+    animation = new AnimationController(
+      vsync: this,
+      duration: new Duration(seconds: 3),
+    );
+    animation.addListener(() {
+      this.setState(() {});
+    });
+    _statusList = [];
   }
 
   void _initBoard() async {
     setState(() => _isLoading = true);
-    _status1 = data.map((a)=>Stext1.Active).toList(growable: false);
-    _status2 = data.map((a)=>Stext2.InActive).toList(growable: false);
-    //  _status1 = data.map((a)=>Stext1.Active).toList(growable: false);
-    // _status2 = data.map((a)=>Stext2.InActive).toList(growable: false);
-
-
-  }
-   Widget _plyer1Side(BuildContext context, double buttonPadding) {
-    var j = 0, h = 0, k = 100;
-    return new ResponsiveGridView(
-      rows: 2,
-      cols: 6,
-      children: data.map((e) => Padding(
-              padding: EdgeInsets.all(buttonPadding),
-              child: _buildItem(j=0, e,_status1[j],_status2[j++]))).toList(growable: false),
-    );
   }
 
-  Widget _plyer2Side(BuildContext context, double buttonPadding) {
-    var j = 0, h = 0, k = 100;
-    return new ResponsiveGridView(
-      rows: 2,
-      cols: 6,
-      children: data1.map((e) => Padding(
-              padding: EdgeInsets.all(buttonPadding),
-              child: _buildItem(j=0, e,_status1[j],_status2[j++]))).toList(growable: false),
-    );
+  Widget _playerKeyBoard(BuildContext context, double buttonPadding, String player) {
+
+    if(player == "player1"){
+      var j = 0;
+      return new ResponsiveGridView(
+        rows: 2,
+        cols: 6,
+        children: data
+            .map((e) => Padding(
+                padding: EdgeInsets.all(buttonPadding),
+                child: _buildItem(j++, e, player)))
+            .toList(growable: false),
+      );
+    }else if(player == "player2"){
+      var j = 0;
+      return new ResponsiveGridView(
+        rows: 2,
+        cols: 6,
+        children: data1
+            .map((e) => Padding(
+                padding: EdgeInsets.all(buttonPadding),
+                child: _buildItem(j++, e, player)))
+            .toList(growable: false),
+      );
+    }
+
   }
 
-  Widget _buildItem(int index, String text,Stext1 status,Stext2 status2) {
+
+  Widget _buildItem(int index, String text, String player) {
     return new MyButton(
-        key: new ValueKey<int>(index),
-        index: index,
-        text: text,
-      status: status,
-      status2: status2,
-        color1: 1,
-        onPress: () {
-          count1=count1+1;
-            setState(() {
-              var btnVal = int.parse(text);
-              for (var i = 0; i < dice_tries.length; i++) {
+      key: new ValueKey<int>(index),
+      index: index,
+      text: text,
+      player: player,
+      color1: 1,
+      onPress: () {
+        count1 = count1 + 1;
+        setState(() {
+          print({"player status : ": player});
+          print({"key index value : ": index});
+          var btnVal=-1,sum=0,sub=0;
+          if(text != '' && dice_tries.length == 2){
+            btnVal = int.parse(text);
+            sum = dice_tries[0] + dice_tries[1];
+            sub = dice_tries[0] - dice_tries[1];
+          }
+            print({"manu um": sum});
+            print({"manu sub": sub});
+          if (sub < 0) {
+            sub = -sub;
+            print({"manu sub": sub});
+          }
+          
+          var userControl = false;
+          if(flag == 0 && player == "player1"){
+            userControl = true;
+          }else if(flag == 1 && player == "player2"){
+              userControl = true;
+          }
 
-                sum = sum + dice_tries[i];
-                sub = dice_tries[i] - (sub);
-                print({"manu sum": sum});
-                print({"manu sub": sub});
+
+          if (userControl) {
+            print({"hurry , you are correct user to access keyboard" : player});
+            if (btnVal == sum) {
+              count = 0;
+              dice_tries.removeRange(0, dice_tries.length);
+              _counter = " ";
+              _counter1 = " ";
+              _counter2 = " ";
+              sum = 0;
+              sub = 0;
+            
+              if (flag == 0) {
+                data[index] = '';
+                flag = 1;
+              } else {
+                data1[index] = '';
+                flag = 0;
               }
-              if(sub < 0) {
-                sub = -sub;
+            } else if (sub == btnVal) {
+              count = 0;
+              dice_tries.removeRange(0, dice_tries.length);
+              _counter = " ";
+              _counter1 = " ";
+              _counter2 = " ";
+              sub = 0;
+              sum = 0;
+              if (flag == 0) {
+                data[index] = '';
+                flag = 1;
+              } else {
+                data1[index] = '';
+                flag = 0;
               }
-              _status1.forEach((e){
-                if(e==Stext1.Visible)
-                  {
-                    flag2=1;
-                  } 
-              });
-             _status2.forEach((e){
-               if(e==Stext2.InActive)
-               {
-                 flag2=0;
-               }
-             });
-
-              if(flag2==0) {
-                if (status == Stext1.Active) {
-                  if (btnVal == sum) {
-                    count = 0;
-                    dice_tries.removeRange(0, dice_tries.length);
-                    _counter = " ";
-                    _counter1 = " ";
-                    _counter2 = " ";
-                    _counter3 = " ";
-                    sum = 0;
-                    sub = 0;
-
-                   setState(() {
-                     _status1[index] = Stext1.Visible;
-                   });
-                    if (flag == 0) {
-                      flag = 1;
-                    }
-                    else {
-                      flag = 0;
-                    }
-                  }
-
-                  else if (sub == btnVal) {
-                    // flag = 1;
-                    count = 0;
-                    dice_tries.removeRange(0, dice_tries.length);
-                    _counter = " ";
-                    _counter1 = " ";
-                    _counter2 = " ";
-                    _counter3 = " ";
-                    sub = 0;
-                    sum = 0;
-                    if (flag == 0) {
-                      flag = 1;
-                    }
-                    else {
-                      flag = 0;
-                    }
-                  }
-                  // else {
-                  //   sum = 0;
-                  // }
-                }
-              }
-              else {
-                print("mannu is data is");
-                if (status == Stext1.Active) {
-                  if (btnVal == sum) {
-                    count = 0;
-                    dice_tries.removeRange(0, dice_tries.length);
-                    _counter = " ";
-                    _counter1 = " ";
-                    _counter2 = " ";
-                    _counter3 = " ";
-                    sum = 0;
-                    sub = 0;
-                    setState(() {
-                      _status1[index] = Stext1.Visible;
-                      _status2[index]=Stext2.Right;
-                    });
-//                setState(() {
-//                  text[i] = null;
-//                });
-                    if (flag == 0) {
-                      flag = 1;
-                    }
-                    else {
-                      flag = 0;
-                    }
-                  }
-
-                  else if (sub == btnVal) {
-                    flag = 1;
-                    count = 0;
-                    dice_tries.removeRange(0, dice_tries.length);
-                    _counter = " ";
-                    _counter1 = " ";
-                    _counter2 = " ";
-                    _counter3 = " ";
-                    sub = 0;
-                    sum = 0;
-                    if (flag == 0) {
-                      flag = 1;
-                    }
-                    else {
-                      flag = 0;
-                    }
-                  }
-                  // else {
-                  //   sum = 0;
-                  // }
-
-                  _status2 = data.map((a)=>Stext2.InActive).toList(growable: false);
-                  _status1 = data1.map((a)=>Stext1.Active).toList(growable: false);
-                }
-                flag2=0;
-              }
-              print("hellow manuuuu $text");
-            });
-
-        },
-        );
+            }
+          }
+          print("hellow manuuuu $text");
+        });
+      },
+    );
   }
 
   void _randomVal() {
     setState(() {
-      if(flag==0) {
+      animation.forward(from: 0.0);
+      if (flag == 0) {
         if (count <= 1) {
           count = count + 1;
           final _random = new Random();
@@ -280,9 +225,8 @@ class DiceGameState extends State<Dice> {
           _counter = "$_counter" + "$dElement" + ",";
           print("dice data $_counter ");
         }
-      }
-      else {
-        if(count <= 1) {
+      } else {
+        if (count <= 1) {
           count = count + 1;
           final _random = new Random();
           var dElement = diceData[_random.nextInt(diceData.length)];
@@ -290,10 +234,10 @@ class DiceGameState extends State<Dice> {
           var randval = int.parse(dElement);
           dice_tries.add(randval);
           _counter1 = dElement;
-          _counter3 = "$_counter3" + "$dElement" + ",";
-          print("dice data $_counter3 ");
+          _counter2 = "$_counter2" + "$dElement" + ",";
+          print("dice data $_counter2 ");
         }
-    };
+      }
     });
   }
 
@@ -301,91 +245,91 @@ class DiceGameState extends State<Dice> {
   Widget build(BuildContext context) {
     MediaQueryData media = MediaQuery.of(context);
     return new LayoutBuilder(builder: (context, constraints) {
-    
-     final hPadding = pow(constraints.maxWidth / 150.0, 2);
+      final hPadding = pow(constraints.maxWidth / 150.0, 2);
       final vPadding = pow(constraints.maxHeight / 150.0, 2);
 
       double maxWidth = (constraints.maxWidth - hPadding * 2) / 6;
 
-      double maxHeight =(constraints.maxHeight - vPadding * 8) / 6;
+      double maxHeight = (constraints.maxHeight - vPadding * 2) / 6;
 
       final buttonPadding = sqrt(min(maxWidth, maxHeight) / 5);
-
-
 
       maxWidth -= buttonPadding * 2;
       maxHeight -= buttonPadding * 2;
       UnitButton.saveButtonSize(context, 2, maxWidth, maxHeight);
       AppState state = AppStateContainer.of(context).state;
 
-    return new Container(
-      padding:
+      return new Container(
+          padding:
               EdgeInsets.symmetric(vertical: vPadding, horizontal: hPadding),
-        color: Colors.white,
-        child: new Column(
-           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            // crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            new Expanded(
-               child: _plyer1Side(context, buttonPadding),
-            ),
-            new Row(
-           mainAxisAlignment: MainAxisAlignment.center,
-           crossAxisAlignment: CrossAxisAlignment.center,
-         children: <Widget>[
-           new Container(
-              height: media.size.height > media.size.width ?constraints.maxHeight*.08 : constraints.maxHeight*.1 ,
-               width: media.size.height < media.size.width? constraints.maxWidth*.1 : constraints.maxWidth*.2 ,
-             color: Colors.red,
-               margin: EdgeInsets.only(right: 20.0),
-               child: new Center(
-                   child: new Text("$_counter",
-                       style: new TextStyle(
-                           color: Colors.black, fontSize: 25.0)))
-           ),
-       new InkWell(
-          onTap: _randomVal,
-           child: new Container(
-              height: media.size.height > media.size.width ?constraints.maxHeight*.15 : constraints.maxHeight*.2 ,
-               width: media.size.height < media.size.width? constraints.maxWidth*.15 : constraints.maxWidth*.2 ,
+          color: Colors.white,
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              new Expanded(
+                child: _playerKeyBoard(context, buttonPadding, "player1"),
+              ),
+              new Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    new Container(
+                        height: media.size.height > media.size.width
+                            ? constraints.maxHeight * .08
+                            : constraints.maxHeight * .1,
+                        width: media.size.height < media.size.width
+                            ? constraints.maxWidth * .1
+                            : constraints.maxWidth * .2,
+                        color: Colors.red,
+                        margin: EdgeInsets.only(right: 20.0),
+                        child: new Center(
+                            child: new Text("$_counter",
+                                style: new TextStyle(
+                                    color: Colors.black, fontSize: 25.0)))),
+                    new InkWell(
+                        onTap: _randomVal,
+                        child: new Container(
+                          height: media.size.height > media.size.width
+                              ? constraints.maxHeight * .15
+                              : constraints.maxHeight * .2,
+                          width: media.size.height < media.size.width
+                              ? constraints.maxWidth * .15
+                              : constraints.maxWidth * .2,
+                          child: new Image(
+                              image: new AssetImage(
+                            animation.isAnimating
+                                ? 'assets/dice_game/dice2.gif'
+                                : 'assets/dice_game/$_counter1.png',
 
-              // decoration: new BoxDecoration(
-              //   color: const Color(0xFFF1F8E9),
-              //   boxShadow: [new BoxShadow(
-              //     color: Colors.green,
-              //     blurRadius: 5.0,
-              //   ),
-              //   ],
-              //   border: new Border.all(
-              //     color: Colors.black,
-              //     width: 5.0,
-              //   ),
-              // ),
-              child: new Image(
-
-              image:new AssetImage('assets/$_counter1.png')
-
-
-              //style: new TextStyle(fontSize: 50.0))
-            ))),
-           new Container(
-           height: media.size.height > media.size.width ?constraints.maxHeight*.08 : constraints.maxHeight*.1 ,
-               width: media.size.height < media.size.width? constraints.maxWidth*.1 : constraints.maxWidth*.2 ,
-             color: Colors.blue,
-               margin: EdgeInsets.only(left: 20.0),
-               child: new Center(
-                   child: new Text("$_counter3",
-                       style: new TextStyle(
-                           color: Colors.black, fontSize: 25.0)))
-
-           ),
-            ]),
-            new Expanded(
-               child: _plyer2Side(context, buttonPadding),
-            ),
-          ],
-        ));
+                          )),
+                        )),
+                    new Container(
+                        height: media.size.height > media.size.width
+                            ? constraints.maxHeight * .08
+                            : constraints.maxHeight * .1,
+                        width: media.size.height < media.size.width
+                            ? constraints.maxWidth * .1
+                            : constraints.maxWidth * .2,
+                        color: Colors.blue,
+                        margin: EdgeInsets.only(left: 20.0),
+                        child: new Center(
+                            child: new Text("$_counter2",
+                                style: new TextStyle(
+                                    color: Colors.black, fontSize: 25.0)))),
+                  ]),
+              new Expanded(
+                child: _playerKeyBoard(context, buttonPadding, "player2"),
+              ),
+            ],
+          ));
     });
+  }
+
+  @override
+  void dispose() {
+    animation.dispose();
+    super.dispose();
   }
 }
 
@@ -398,19 +342,17 @@ class MyButton extends StatefulWidget {
       this.flag,
       this.onAccepted,
       this.code,
+      this.player,
       this.isRotated,
       this.img,
-        this.status,
-        this.status2,
-        this.onPress,
+      this.onPress,
       this.keys})
       : super(key: key);
   final index;
   final int color1;
   final int flag;
   final int code;
-  Stext1 status;
-  Stext2 status2;
+  String player;
   final bool isRotated;
   final String text;
   final String img;
@@ -465,12 +407,10 @@ class _MyButtonState extends State<MyButton> with TickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-//    MediaQueryData media = MediaQuery.of(context);
     return new ScaleTransition(
         scale: animation,
-        child:new Container(
-          // color: Colors.white,
-        child: new UnitButton(
+        child: new Container(
+            child: new UnitButton(
           onPress: widget.onPress,
           text: widget.text,
           unitMode: UnitMode.text,
