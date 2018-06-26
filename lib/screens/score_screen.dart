28ab34fb571f6b1595b322d9f977771bc8ef4215
Karan Saluja <35517558+KaranChimple@users.noreply.github.com>
2018-06-26 16:18:43 +0530

    star overflow issue resolvedfor smaller screens

--- lib/screens/score_screen.dart
+++ lib/screens/score_screen.dart
@@ -128,13 +128,16 @@ class _ScoreScreenState extends State<ScoreScreen>
     
   }
 
-  Widget _buildItem(int index, String text) {
-    return new MyButton(
+  Widget _buildItem(int index, String text, double ht, double wd) {
+    return new LimitedBox(
+      maxHeight: ht * 0.2,
+      maxWidth: wd * 0.15,
+      child: new MyButton(
         key: new ValueKey<int>(index),
         text: text,
         keys: keys++,
         onPress: () {}
-        );
+        ));
   }
   
 
@@ -181,12 +184,12 @@ class _ScoreScreenState extends State<ScoreScreen>
     return new LayoutBuilder(builder: (context, constraints) {
       print("flag = $flag");
     List <Widget> starsMap1 =  mystars
-                              .map((e) => _buildItem(j++, e),)
+                              .map((e) => _buildItem(j++, e, ht, wd),)
                               .toList(growable: false);
 
 
     List <Widget> starsMap2 =  otherstars
-                              .map((e) => _buildItem(k++, e),)
+                              .map((e) => _buildItem(k++, e, ht, wd),)
                               .toList(growable: false);
     for(var i=0; i < 4; i++){
     tablestars1.add(new ScaleTransition(
@@ -271,26 +274,22 @@ class _ScoreScreenState extends State<ScoreScreen>
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
                   new  Row(
-                    mainAxisAlignment: gameDisplay == GameDisplay.myHeadToHead ? MainAxisAlignment.spaceBetween : MainAxisAlignment.center,
+                    mainAxisAlignment: gameDisplay == GameDisplay.myHeadToHead || gameDisplay == GameDisplay.networkTurnByTurn || gameDisplay == GameDisplay.localTurnByTurn ? MainAxisAlignment.spaceAround : MainAxisAlignment.center,
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: <Widget>[
-                      new Container(
-                        width: wd * 0.49,
-                        child: new Row(
-                        mainAxisAlignment: gameDisplay == GameDisplay.myHeadToHead ? MainAxisAlignment.center : MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: tablestars1)),
+                      new Row(
+                      mainAxisAlignment: gameDisplay == GameDisplay.myHeadToHead ? MainAxisAlignment.center : MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: tablestars1),
 
                         new Padding(
                           padding: new EdgeInsets.all(10.0),
                         ),
                         
-                       gameDisplay == GameDisplay.myHeadToHead || gameDisplay == GameDisplay.networkTurnByTurn || gameDisplay == GameDisplay.localTurnByTurn ? new Container(
-                         width: wd * 0.49,
-                         child: new Row(
+                       gameDisplay == GameDisplay.myHeadToHead || gameDisplay == GameDisplay.networkTurnByTurn || gameDisplay == GameDisplay.localTurnByTurn ? new Row(
                         mainAxisAlignment: MainAxisAlignment.end,
                         crossAxisAlignment: CrossAxisAlignment.end,
-                        children: tablestars2)) : new Row(),
+                        children: tablestars2) : new Row(),
                         ]),
                 new Row(
                   mainAxisAlignment: gameDisplay == GameDisplay.myHeadToHead || gameDisplay == GameDisplay.networkTurnByTurn || gameDisplay == GameDisplay.localTurnByTurn ? MainAxisAlignment.spaceAround : MainAxisAlignment.center,
@@ -446,25 +445,12 @@ class _MyButtonState extends State<MyButton> with TickerProviderStateMixin {
     // _play();
     return new Shake(
       animation: animation,
-      child: new GestureDetector(
-      child: new Padding(
-        padding: new EdgeInsets.only(left: 5.0),
-        child: new Container(  
-        height: ht > wd ? ht * 0.1 : ht * 0.13,
-        width: ht > wd ? wd * 0.15 : wd * 0.12,     
-      child: new FlatButton(
-         onPressed: () => widget.onPress(),
-         color: Colors.transparent, 
-         highlightColor: Colors.black,  
-         disabledColor: Colors.black,
-         splashColor: Colors.black,      
-         child: new IconButton(
+      child: new IconButton(
         icon: _displayText == "true" ? new Image.asset("assets/star_gained.png") : new Image.asset("assets/star.png"),
         key: new Key("${widget.keys}"),
-        iconSize: ht > wd ? ht * 0.1 : wd * 0.1,        color: Colors.black,
+        iconSize: ht > wd ? ht * 0.01 : wd * 0.05,        
+        color: Colors.black,
         onPressed: () {},
-         )         
-    )))
-    ));
+      ));
   }
 }
