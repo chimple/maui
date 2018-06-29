1a195cd4db69139f212608d13871855e01fd6009
Rishabh Singh <rishabh@sutara.org>
2018-06-29 12:42:37 +0530

    Code Updated

--- lib/games/clue_game.dart
+++ lib/games/clue_game.dart
@@ -28,7 +28,6 @@ class ClueGame extends StatefulWidget {
   @override
   _ClueGameState createState() => new _ClueGameState();
 }
-
 class _ClueGameState extends State<ClueGame> with TickerProviderStateMixin {
   AnimationController controller;
   Animation<double> animation, noanimation;
@@ -51,21 +50,19 @@ class _ClueGameState extends State<ClueGame> with TickerProviderStateMixin {
   List<String> _categoryup=[];
   List<String> _categorydown=[];
   List<String> categoryName = [];
-  List<String> newlist = [];
   List<String> listOfThings = [];
+  List<String> listOfThingscopy = [];
   List<String> listOfSyllables =[];
-  List<String> listOfSyllablesremove =[];
-  List<String> listOfSyllablescopy = [];
-  List<String> newSyllables = [];
+  List<String> listOfSyllablescopy =[];
   List<String> dummylist = ['eq','ag','fg','cv','bn','mk','lk','asd','po','qw','tyu','uy','cb','ni','oiu','kjh','ko','za','aq'];
   bool _isLoading = true;
-  String str = '';
   Map<String, List<String>> data1;
   Map<String, Map<String, List<String>>> data;
   void _initClueGame() async{
      listOfSyllables.clear();
      listOfThings.clear();
      categoryName.clear();
+     listOfThingscopy.clear();
      setState(() => _isLoading = true);
       data = await fetchClueGame(widget.gameCategoryId);
       data.forEach((k, data1) {
@@ -77,37 +74,34 @@ class _ClueGameState extends State<ClueGame> with TickerProviderStateMixin {
         });
       });
     });
+    reset(word);
+    listOfThingscopy.addAll(listOfThings);
     _categoryup = categoryName.sublist(0, 2);
     _categorydown = categoryName.sublist(2, 4);
-      listOfSyllables.addAll(dummylist);
-    print('list of syllables is $listOfSyllables'); 
-    print('word is vefore $words');
-    try{
-        words.removeLast();
-    }
-    catch (Exception ) {
-      
-    }
-    newlist.addAll(words);
-    for (int i = 0; i < newlist.length; i++) listOfSyllables.remove(newlist[i]);
-   // listOfSyllablesremove=listOfSyllables;
-    // print('list of syll.remv is $listOfSyllables');
-    // print('newlist is $newlist');
-    // print('word is in data $words');
-    //print('word after is $words');
-    listOfSyllablescopy = listOfSyllables.sublist(0, 19);
-    listOfSyllablescopy.shuffle(); 
     setState(() => _isLoading = false);
   }
- 
+  void reset( String name){
+     listOfSyllables.clear();
+     listOfThings.clear();
+     data.forEach((k, data1) {
+      categoryName.add(k);
+      data1.remove(name);
+       data1.forEach((a, list) {
+        listOfThings.add(a);
+        listOfThings.remove(name);
+        list.forEach((c) {
+          listOfSyllables.add(c);
+        });
+      });
+    });
+     listOfSyllables.addAll(dummylist);
+    listOfSyllablescopy = listOfSyllables.sublist(0, 19);
+    listOfSyllablescopy.shuffle(); 
+  }
  void _validate() {
      String word = _result;
-        if(word=='')
-     {
-
-     }
-    else  
-    if (listOfThings.sublist(0, 3).contains(_result)) {
+    if(word=='')  {}
+   else if (listOfThingscopy.sublist(0, 3).contains(_result)) {
       setState(() {
         _result = 'you Type Drink';
          count++;
@@ -127,11 +121,11 @@ class _ClueGameState extends State<ClueGame> with TickerProviderStateMixin {
         setState(() {
           _flag = 0;
           _result = '';
-            _initClueGame();
+            reset(word);
         });
         controller.stop();
       });
-    } else if (listOfThings.sublist(3, 6).contains(_result)) {
+    } else if (listOfThingscopy.sublist(3, 6).contains(_result)) {
       setState(() {
         _result = 'you Type Travel';
          count++;
@@ -151,11 +145,11 @@ class _ClueGameState extends State<ClueGame> with TickerProviderStateMixin {
         setState(() {
           _flag = 0;
           _result = '';
-            _initClueGame();
+          reset(word);
         });
         controller.stop();
       });
-    } else if (listOfThings.sublist(6, 9).contains(_result)) {
+    } else if (listOfThingscopy.sublist(6, 9).contains(_result)) {
       setState(() {
         _result = 'you Type Red Fruit';
          count++;
@@ -175,11 +169,11 @@ class _ClueGameState extends State<ClueGame> with TickerProviderStateMixin {
         setState(() {
           _flag = 0;
           _result = '';
-            _initClueGame();
+            reset(word);
         });
         controller.stop();
       });
-    } else if (listOfThings.sublist(9, 12).contains(_result)) {
+    } else if (listOfThingscopy.sublist(9, 12).contains(_result)) {
       setState(() {
         _result = 'you Type black Pet';
          count++;
@@ -199,13 +193,12 @@ class _ClueGameState extends State<ClueGame> with TickerProviderStateMixin {
         setState(() {
           _flag = 0;
           _result = '';
-            _initClueGame();
+          reset(word);
         });
         controller.stop();
       });
     } else {
       toAnimateFunction();
-      words.clear();
       new Future.delayed(const Duration(milliseconds: 1000), () {
         setState(() {
           _flag = 0;
@@ -412,25 +405,12 @@ class _ClueGameState extends State<ClueGame> with TickerProviderStateMixin {
         onPress: setData); 
   }
   List<int> lengthofwords = [];
-   List<String> words = [];
-   List<String> correctwords = [];
-   int ind;
   void setData(String a) {
     setState(() {
        len = a.length;
       lengthofwords.add(len);
-      correctwords.add(a);
       _result = _result + a;
-      ind = correctwords.indexOf(a);
-      if(listOfThings.contains(_result))
-      {
-         words.add(a);
-         words.addAll(correctwords.sublist(0,ind));
-         
-      }
-      // print('correctword is $correctwords');
-      // print('word is $words');
-    });  //words.removeLast();
+    });  
     }
   int j = 0;
  }
