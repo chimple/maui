ac53f72d0a0ce4ecdad3d24827b2420ac533265b
SATISH <36834824+satish754ss@users.noreply.github.com>
2018-07-13 17:49:00 +0530

    Updated Codes for Camera and Spin Wheel

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
-
 import 'dart:io';
-
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:maui/components/Shaker.dart';
@@ -10,9 +9,9 @@ import 'package:maui/components/user_list.dart';
 import 'package:maui/db/entity/user.dart';
 import 'package:maui/repos/user_repo.dart';
 import 'package:maui/state/app_state_container.dart';
-
+import 'package:image_picker/image_picker.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
 import 'tab_home.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -80,18 +79,22 @@ class _LoginScreenState extends State<LoginScreen>
     Navigator.of(context).pushNamed('/camera');
   }
 
+  String imagePath;
+  bool displaImage = true;
+  Future getImageOriganl() async {
+    String image = await ImagePick.pickImage(source: ImageSrc.camera);
+    print("image path $image");
+    setState(() {
+      imagePath = image;
+      displaImage = false;
+    });
+  }
+
   Orientation ornt;
   @override
   Widget build(BuildContext context) {
     var user = AppStateContainer.of(context).state.loggedInUser;
     print("user detail ?::: $user");
-    //if (user == null) {
-
-    //}
-    //  else {
-    //SystemChrome.setPreferredOrientations([]);
-    // }
-
     return (user != null)
         ? new TabHome()
         : new Scaffold(
@@ -117,18 +120,19 @@ class _LoginScreenState extends State<LoginScreen>
                           mainAxisSize: MainAxisSize.max,
                           // mainAxisAlignment: MainAxisAlignment.spaceAround,
                           children: <Widget>[
-                            imagePathStore == null
+                            imagePath == null
                                 ? Center(
                                     child: Container(
                                       height: 130.0,
                                       width: 130.0,
                                       child: RaisedButton(
+                                        splashColor: Colors.blue,
                                         color: Colors.white,
                                         shape: CircleBorder(
                                             side: BorderSide(
                                                 width: 3.0,
                                                 color: new Color(0xff4C5C9E))),
-                                        onPressed: () => getImage(context),
+                                        onPressed: getImageOriganl,
                                         child: new IconTheme(
                                           data: IconThemeData(
                                               size: 70.0,
@@ -138,16 +142,19 @@ class _LoginScreenState extends State<LoginScreen>
                                       ),
                                     ),
                                   )
-                                : new Container(
-                                    width: 130.0,
-                                    height: 130.0,
-                                    decoration: new BoxDecoration(
-                                        shape: BoxShape.circle,
-                                        image: new DecorationImage(
-                                          image: new FileImage(
-                                              new File(imagePathStore)),
-                                          fit: BoxFit.fill,
-                                        ))),
+                                : InkWell(
+                                    onTap: getImageOriganl,
+                                    child: new Container(
+                                        width: 130.0,
+                                        height: 130.0,
+                                        decoration: new BoxDecoration(
+                                            shape: BoxShape.circle,
+                                            image: new DecorationImage(
+                                              image: new FileImage(
+                                                  new File(imagePath)),
+                                              fit: BoxFit.fill,
+                                            ))),
+                                  ),
                             new TextField(
                               autocorrect: false,
                               onSubmitted: _submit(userName),
@@ -206,9 +213,9 @@ class _LoginScreenState extends State<LoginScreen>
   }
 
   void tabSreen() async {
-    if (imagePathStore != null && userName != '' && userName != null) {
+    if (imagePath != null && userName != '' && userName != null) {
       var user = await new UserRepo().insertLocalUser(
-          new User(image: imagePathStore, currentLessonId: 1, name: userName));
+          new User(image: imagePath, currentLessonId: 1, name: userName));
       AppStateContainer.of(context).setLoggedInUser(user);
       //Navigator.of(context).pop();
     } else {
@@ -225,5 +232,42 @@ class _LoginScreenState extends State<LoginScreen>
         controller.stop();
       });
     }
+  }
+}
+
+enum ImageSrc {
+  camera,
+  galery,
+}
+
+class ImagePick {
+  static const MethodChannel _channel =
+      const MethodChannel('plugins.flutter.io/image_picker');
+  static Future<String> pickImage({
+    @required ImageSrc source,
+    double maxWidth,
+    double maxHeight,
+  }) async {
+    assert(source != null);
+
+    List<CameraDescription> cameras;
+    if (maxWidth != null && maxWidth < 0) {
+      print("camera sdede $cameras");
+      throw new ArgumentError.value(maxWidth, 'maxWidth cannot be negative');
+    }
+
+    if (maxHeight != null && maxHeight < 0) {
+      throw new ArgumentError.value(maxHeight, 'maxHeight cannot be negative');
+    }
+    final String path = await _channel.invokeMethod(
+      'pickImage',
+      <String, dynamic>{
+        'source': source.index,
+        'maxWidth': maxWidth,
+        'maxHeight': maxHeight,
+      },
+    );
+
+    return path == null ? null : path;
   }
 }
