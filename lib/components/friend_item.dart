57dceccc6ae19de7e20641cd8886a543dc123970
Srikanth Talapadi <sri@zoolore.org>
2018-07-20 14:39:14 +0530

    friend item was nulling out

--- lib/components/friend_item.dart
+++ lib/components/friend_item.dart
@@ -39,56 +39,61 @@ class FriendItem extends StatelessWidget {
         child: new Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
-          children:[ numNotifs > 0
-              ? Badge(
-                  color: Colors.red,
-                  textStyle:
-                      const TextStyle(fontSize: 30.0, color: Colors.white),
-                  borderSize: 5.0,
-                  borderColor: Colors.red,
-                  value: '$numNotifs',
-                  child: _buildFriendItem(id, user))
-              : _buildFriendItem(id, user),
-              new Text(name, style: new TextStyle(fontSize: 20.0, color: Colors.white, fontWeight: FontWeight.bold),),
-        ],));
+          children: [
+            numNotifs > 0
+                ? Badge(
+                    color: Colors.red,
+                    textStyle:
+                        const TextStyle(fontSize: 30.0, color: Colors.white),
+                    borderSize: 5.0,
+                    borderColor: Colors.red,
+                    value: '$numNotifs',
+                    child: _buildFriendItem(id, user))
+                : _buildFriendItem(id, user),
+            new Text(
+              name ?? '',
+              style: new TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+            ),
+          ],
+        ));
   }
 
   Widget _buildFriendItem(String id, User user) {
     return new InkWell(
-        onTap: onTap,
-        child: new CircleAvatar(
-              radius: 80.0,
+      onTap: onTap,
+      child: new CircleAvatar(
+        radius: 80.0,
 //                maxRadius: 40.0,
-              backgroundImage:
-                  new AssetImage("assets/chat_image/chat icon_04.png"),
-              child: new Center(
-                child: RawMaterialButton(
-                  elevation: 20.0,
+        backgroundImage: new AssetImage("assets/chat_image/chat icon_04.png"),
+        child: new Center(
+          child: RawMaterialButton(
+            elevation: 20.0,
 
-                  shape: new CircleBorder(
-                    side: new BorderSide(
-                      color: Color(color),
-                      width: 4.0,
-                    ),
-                  ),
-                  child: new CircleAvatar(
-                      radius: 50.0,
-                      child: new Container(
-                          width: 100.0,
-                          height: 100.0,
-                          decoration: new BoxDecoration(
-                              shape: BoxShape.circle,
-                              image: new DecorationImage(
-                                  image: replaceWithHoodie && user.id == id
-                                      ? new AssetImage(
-                                          'assets/chat_Bot_Icon.png')
-                                      : isFile
-                                          ? FileImage(File(imageUrl))
-                                          : imageMemory != null
-                                              ? MemoryImage(imageMemory)
-                                              : AssetImage(
-                                                  'assets/hoodie_bear.png'),
-                                  fit: BoxFit.fill)))),
+            shape: new CircleBorder(
+              side: new BorderSide(
+                color: Color(color),
+                width: 4.0,
+              ),
+            ),
+            child: new CircleAvatar(
+                radius: 50.0,
+                child: new Container(
+                    width: 100.0,
+                    height: 100.0,
+                    decoration: new BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: new DecorationImage(
+                            image: replaceWithHoodie && user.id == id
+                                ? new AssetImage('assets/chat_Bot_Icon.png')
+                                : isFile
+                                    ? FileImage(File(imageUrl))
+                                    : imageMemory != null
+                                        ? MemoryImage(imageMemory)
+                                        : AssetImage('assets/hoodie_bear.png'),
+                            fit: BoxFit.fill)))),
 //                            child: new Container(
 //                                width: 100.0,
 //                                height: 100.0,
@@ -104,8 +109,9 @@ class FriendItem extends StatelessWidget {
 //                                      ? MemoryImage(imageMemory)
 //                                      : AssetImage('assets/hoodie_bear.png'),
 //                                  fit: BoxFit.fill))),
-                ),
-              ),
-            ),);
+          ),
+        ),
+      ),
+    );
   }
 }
