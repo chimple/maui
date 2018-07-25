958692bf88803355409d0c653038ee292791fd5d
nikhil <nikhil@sutara.org>
2018-07-25 16:11:23 +0530

    Fixes #642

---
+++ lib/db/dao/article_dao.dart
@@ -0,0 +1,19 @@
+import 'dart:async';
+
+import 'package:maui/app_database.dart';
+import 'package:maui/db/entity/article.dart';
+import 'package:sqflite/sqflite.dart';
+
+class ArticleDao {
+  Future<Article> getArticle(String id, {Database db}) async {
+    db = db ?? await new AppDatabase().getDb();
+    List<Map> maps = await db.query(Article.table,
+        columns: [Article.idCol, Article.nameCol,Article.topic_idCol,Article.orderCol,Article.videoCol,Article.audioCol, Article.imageCol, Article.textCol],
+        where: '${Article.idCol} = ?',
+        whereArgs: [id]);
+    if (maps.length > 0) {
+      return Article.fromMap(maps.first);
+    }
+    return null;
+  }
+}
