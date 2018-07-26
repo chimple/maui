7efd393d7ae0fbf56025b947631cb486811c53b2
nikhil <nikhil@sutara.org>
2018-07-26 12:57:29 +0530

    updated #642

--- lib/db/dao/article_dao.dart
+++ lib/db/dao/article_dao.dart
@@ -5,7 +5,7 @@ import 'package:maui/db/entity/article.dart';
 import 'package:sqflite/sqflite.dart';
 
 class ArticleDao {
-  Future<Article> getArticle(String id,String topicId, {Database db}) async {
+  Future<Article> getArticle(String id, {Database db}) async {
     db = db ?? await new AppDatabase().getDb();
     List<Map> maps = await db.query(Article.table,
         columns: [
@@ -18,8 +18,8 @@ class ArticleDao {
           Article.textCol,
           Article.orderCol
         ],
-        where: '${Article.idCol} = ? AND ${Article.topicIdCol} = ?',
-        whereArgs: [id,topicId]);
+        where: '${Article.idCol} = ?',
+        whereArgs: [id]);
     if (maps.length > 0) {
       return Article.fromMap(maps.first);
     }
