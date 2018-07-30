2b850e207f39bf7ed50f3a696b9fd880a36ed4d9
sutara-aayush <35102016+sutara-aayush@users.noreply.github.com>
2018-07-30 15:29:23 +0530

    Topic screen Fixed #687

--- lib/screens/topic_screen.dart
+++ lib/screens/topic_screen.dart
@@ -2,46 +2,44 @@ import 'package:flutter/material.dart';
 
 import 'package:maui/repos/article_repo.dart';
 import 'package:maui/db/entity/article.dart';
-import 'package:tuple/tuple.dart';
 
 class TopicScreen extends StatefulWidget {
+  TopicScreen({key, this.topicName, this.topicId}) : super(key: key);
+  String topicName;
+  String topicId;
 
-  TopicScreen({key}):super(key:key);
-  
   @override
   _TopicScreenState createState() => _TopicScreenState();
 }
 
 class _TopicScreenState extends State<TopicScreen> {
-
   List<Article> _articles;
   bool _isLoading = true;
 
   void _initTopic() async {
-    new ArticleRepo().getArticle('lion').then((articles) async{
-      _articles = articles;
+    new ArticleRepo()
+        .getArticlesByTopicId(widget.topicId)
+        .then((articles) async {
+      setState(() {
+        _articles = articles;
+        _isLoading = false;
+      });
     });
   }
 
   @override
-    void initState() {
-      // TODO: implement initState
-      super.initState();
-      setState(() {
-              _initTopic();
-              _isLoading =  false;
-            });
-
-    }
-
-
-
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _initTopic();
+  }
 
   @override
   Widget build(BuildContext context) {
+    print("<<<<<<<<<<<<<<<$_articles>>>>>>>>>>>>>>>>>>>");
     return Scaffold(
       appBar: new AppBar(
-        title: new Text("Topic"),
+        title: new Text(widget.topicName),
         centerTitle: true,
         backgroundColor: Colors.teal[300],
         elevation: 5.0,
@@ -61,39 +59,37 @@ class _TopicScreenState extends State<TopicScreen> {
         ],
       ),
       body: new Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            new Expanded(
-              flex: 1,
-              child: new IconButton(
-                  onPressed: () => print("object"),
-                  icon: new Icon(Icons.arrow_left),
-                  iconSize: 50.0,
-                ),
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          new Expanded(
+            flex: 1,
+            child: new IconButton(
+              onPressed: () => print("object"),
+              icon: new Icon(Icons.arrow_left),
+              iconSize: 50.0,
             ),
-            _isLoading == false ? 
-            new Expanded(
-              flex: 16,
-              child: new Container(
-                color: Colors.green,
-              ),
-            ) :
-            new Expanded(
-              flex: 16,
-              child: new CircularProgressIndicator(),
-            )
-            ,
-            new Expanded(
-              flex: 1,
-              child: new IconButton(
-                  onPressed: () => print("object"),
-                  icon: new Icon(Icons.arrow_right),
-                  iconSize: 50.0,
+          ),
+          _isLoading == false
+              ? new Expanded(
+                  flex: 16,
+                  child: new Container(
+                    color: Colors.green,
+                  ),
+                )
+              : new Expanded(
+                  flex: 16,
+                  child: new CircularProgressIndicator(),
                 ),
-            )
-          ],
-
-        ),
+          new Expanded(
+            flex: 1,
+            child: new IconButton(
+              onPressed: () => print("object"),
+              icon: new Icon(Icons.arrow_right),
+              iconSize: 50.0,
+            ),
+          )
+        ],
+      ),
     );
   }
 }
