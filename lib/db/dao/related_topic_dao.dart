d64c52d8b626e6e8ce1da2deb85f02618eeffc13
nikhil <nikhil@sutara.org>
2018-07-31 14:27:11 +0530

    Fixes #671

--- lib/db/dao/related_topic_dao.dart
+++ lib/db/dao/related_topic_dao.dart
@@ -2,10 +2,11 @@ import 'dart:async';
 
 import 'package:maui/app_database.dart';
 import 'package:maui/db/entity/related_topic.dart';
+import 'package:maui/db/entity/topic.dart';
 import 'package:sqflite/sqflite.dart';
 
 class RelatedTopicDao {
-  Future<List<RelatedTopic>> getRelatedTopics(String topicId,
+  Future<List<RelatedTopic>> getRelatedTopicsByTopicId(String topicId,
       {Database db}) async {
     db = db ?? await new AppDatabase().getDb();
     List<Map> maps = await db.query(RelatedTopic.table,
@@ -19,5 +20,24 @@ class RelatedTopicDao {
     return maps
         .map((el) => new RelatedTopic.fromMap(el))
         .toList(growable: false);
+  }
+
+  Future<List<Topic>> getTopicsByRelatedTopicId(String topicId,
+      {Database db}) async {
+    db = db ?? await new AppDatabase().getDb();
+    List<Map> maps =
+        await db.query('${Topic.table} t, ${RelatedTopic.table} r', columns: [
+      't.${Topic.idCol}',
+      't.${Topic.nameCol}',
+      't.${Topic.imageCol}',
+      't.${Topic.colorCol}'
+    ], where: '''
+t.${Topic.idCol} != ? 
+AND (r.${RelatedTopic.topicIdCol} = ? OR r.${RelatedTopic.relTopicIdCol} = ?)
+''', whereArgs: [
+      topicId,
+      topicId
+    ]);
+    return maps.map((el) => new Topic.fromMap(el)).toList();
   }
 }
--- lib/repos/related_topic_repo.dart
+++ lib/repos/related_topic_repo.dart
@@ -2,13 +2,18 @@ import 'dart:async';
 
 import 'package:maui/db/entity/related_topic.dart';
 import 'package:maui/db/dao/related_topic_dao.dart';
+import 'package:maui/db/entity/topic.dart';
 
 class RelatedTopicRepo {
   static final RelatedTopicDao relTopicDao = RelatedTopicDao();
 
   const RelatedTopicRepo();
 
-  Future<List<RelatedTopic>> getRelatedTopics(String id) async {
-    return relTopicDao.getRelatedTopics(id);
+  Future<List<RelatedTopic>> getRelatedTopicsByTopicId(String topicId) async {
+    return relTopicDao.getRelatedTopicsByTopicId(topicId);
+  }
+
+  Future<List<Topic>> getTopicsByRelatedTopicId(String topicId) async {
+    return relTopicDao.getTopicsByRelatedTopicId(topicId);
   }
 }
