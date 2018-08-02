5b3adac779c3bb4329884bd7b6a59da0e1d2a84d
Karan Saluja <karan@sutara.org>
2018-08-02 14:03:34 +0530

    trueorfalse entity creation

---
+++ lib/db/entity/trueOrFalse.dart
@@ -0,0 +1,9 @@
+class Topic {
+  static const table = 'trueOrFalse';
+  static const id = 'id';
+  static const topicId = 'topicId';
+  static const question = 'question';
+  static const associatedQuestion = 'associatedQUestion';
+  static const isTrue = 'isTrue';
+
+}
