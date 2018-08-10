e777e9ba9502c518df3b0461af57fbd98fa64b68
Rajesh-Chimple <35517566+Rajesh-Chimple@users.noreply.github.com>
2018-08-10 16:23:09 +0530

    Fixes #738 Made the requested changes

--- lib/quiz/grouping_quiz.dart
+++ lib/quiz/grouping_quiz.dart
@@ -38,28 +38,19 @@ class _GroupingQuizState extends State<GroupingQuiz> {
   }
 
   void initData() {
-    testMap.forEach((k, v) {
-      if (k == 'image') {
-        image = v;
-      }
-      if (k == 'question') {
-        question = v;
-      }
-      if (k == 'groups') {
-        v.map((f) {
-          options.add(f);
-        }).toList(growable: false);
-        ;
-      }
+    image = testMap['image'];
+    question = testMap['question'];
+    options = testMap['groups'];
+
+    options[0].forEach((f) {
+      itemsOfgroupA.add(f);
+      allOptions.add(f);
     });
-    for (int i = 0; i < options[0].length; i++) {
-      itemsOfgroupA.add(options[0][i]);
-      allOptions.add(options[0][i]);
-    }
-    for (int i = 0; i < options[1].length; i++) {
-      itemsOfgroupB.add(options[1][i]);
-      allOptions.add(options[1][i]);
-    }
+
+    options[1].forEach((f) {
+      itemsOfgroupB.add(f);
+      allOptions.add(f);
+    });
 
     shuffledOptions.addAll(
         allOptions.take(allOptions.length).toList(growable: false)..shuffle());
@@ -73,9 +64,18 @@ class _GroupingQuizState extends State<GroupingQuiz> {
     print("shuffledOptions: ${shuffledOptions}");
   }
 
+  void _removeData(String str) {
+    print("Call Back..!!");
+    setState(() {
+      shuffledOptions.remove(str);
+    });
+    print("shuffledOptions after remove() $str: ${shuffledOptions}");
+  }
+
   @override
   Widget build(BuildContext context) {
     return new GameUI(
+        removeData: _removeData,
         question: question,
         scrollControllerForGroupA: _scrollControllerForGroupA,
         optionsOfGroupA: optionsOfGroupA,
@@ -85,7 +85,17 @@ class _GroupingQuizState extends State<GroupingQuiz> {
   }
 }
 
-class GameUI extends StatefulWidget {
+class GameUI extends StatelessWidget {
+  final Function(String) removeData;
+  final String question;
+  final ScrollController scrollControllerForGroupA;
+  final List<String> optionsOfGroupA;
+  final List<String> shuffledOptions;
+  final ScrollController scrollControllerForGroupB;
+  final List<String> optionsOfGroupB;
+  final String groupA = 'GroupA';
+  final String groupB = 'GroupB';
+
   const GameUI({
     Key key,
     @required this.question,
@@ -94,22 +104,9 @@ class GameUI extends StatefulWidget {
     @required this.shuffledOptions,
     @required this.scrollControllerForGroupB,
     @required this.optionsOfGroupB,
+    @required this.removeData,
   }) : super(key: key);
 
-  final String question;
-  final ScrollController scrollControllerForGroupA;
-  final List<String> optionsOfGroupA;
-  final List<String> shuffledOptions;
-  final ScrollController scrollControllerForGroupB;
-  final List<String> optionsOfGroupB;
-
-  @override
-  GameUIState createState() {
-    return new GameUIState();
-  }
-}
-
-class GameUIState extends State<GameUI> {
   @override
   Widget build(BuildContext context) {
     return new Container(
@@ -127,7 +124,7 @@ class GameUIState extends State<GameUI> {
               color: Colors.red,
               child: Center(
                 child: Text(
-                  widget.question,
+                  question,
                   style: TextStyle(
                     color: Colors.white,
                     decoration: TextDecoration.none,
@@ -144,160 +141,21 @@ class GameUIState extends State<GameUI> {
                 mainAxisSize: MainAxisSize.min,
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
-                  new Column(
-                    children: <Widget>[
-                      Center(
-                        child: Text(
-                          'Group A',
-                          style: TextStyle(
-                            color: Colors.black,
-                            decoration: TextDecoration.none,
-                            fontSize: 20.0,
-                          ),
-                        ),
-                      ),
-                      new DragTarget(
-                        onAccept: (String label) {
-                          SchedulerBinding.instance.addPostFrameCallback((_) {
-                            widget.scrollControllerForGroupA.animateTo(
-                              widget.scrollControllerForGroupA.position
-                                  .maxScrollExtent,
-                              duration: const Duration(milliseconds: 300),
-                              curve: Curves.easeOut,
-                            );
-                          });
-
-                          if (widget.optionsOfGroupA.contains(label)) {
-                            return;
-                          } else {
-                            widget.optionsOfGroupA.add(label);
-                            setState(() {
-                              widget.shuffledOptions.remove(label);
-                            });
-                            print("shuffledOptions after remove(): ${widget.shuffledOptions}");
-                          }
-                        },
-                        builder: (BuildContext context, List<dynamic> accepted,
-                            List<dynamic> rejected) {
-                          return new Container(
-                            width: 200.0,
-                            height: 380.0,
-                            decoration: new BoxDecoration(
-                              shape: BoxShape.rectangle,
-                              color: Colors.blue,
-                              borderRadius: new BorderRadius.all(
-                                  new Radius.circular(20.0)),
-                            ),
-                            margin: const EdgeInsets.only(left: 0.0),
-                            child: Center(
-                                child: new ListView.builder(
-                              controller: widget.scrollControllerForGroupA,
-                              itemCount: widget.optionsOfGroupA?.length,
-                              itemBuilder: (context, i) => new Container(
-                                    margin: const EdgeInsets.all(7.0),
-                                    decoration: new BoxDecoration(
-                                      border: new Border.all(
-                                          color: Colors.black, width: 3.0),
-                                      color: Colors.orange,
-                                      boxShadow: [
-                                        new BoxShadow(
-                                          color: const Color(0x44000000),
-                                          spreadRadius: 2.0,
-                                          offset: const Offset(0.0, 1.0),
-                                        )
-                                      ],
-                                      borderRadius:
-                                          new BorderRadius.circular(12.0),
-                                    ),
-                                    child: new ListTile(
-                                      title: new Text(widget.optionsOfGroupA[i],
-                                          textAlign: TextAlign.center),
-                                    ),
-                                  ),
-                            )),
-                          );
-                        },
-                      ),
-                    ],
-                  ),
+                  new GroupUI(
+                      removeData: removeData,
+                      group: groupA,
+                      optionsOfGroup: optionsOfGroupA,
+                      scrollControllerForGroup: scrollControllerForGroupA,
+                      shuffledOptions: shuffledOptions),
                   new Padding(
                     padding: const EdgeInsets.all(5.0),
                   ),
-                  new Column(
-                    children: <Widget>[
-                      Center(
-                        child: Text(
-                          'Group B',
-                          style: TextStyle(
-                            color: Colors.black,
-                            decoration: TextDecoration.none,
-                            fontSize: 20.0,
-                          ),
-                        ),
-                      ),
-                      new DragTarget(
-                        onAccept: (String label) {
-                          SchedulerBinding.instance.addPostFrameCallback((_) {
-                            widget.scrollControllerForGroupB.animateTo(
-                              widget.scrollControllerForGroupB.position
-                                  .maxScrollExtent,
-                              duration: const Duration(milliseconds: 300),
-                              curve: Curves.easeOut,
-                            );
-                          });
-
-                          if (widget.optionsOfGroupB.contains(label)) {
-                            return;
-                          } else {
-                            setState(() {
-                              widget.shuffledOptions.remove(label);
-                            });
-                            widget.optionsOfGroupB.add(label);
-                          }
-                        },
-                        builder: (BuildContext context, List<dynamic> accepted,
-                            List<dynamic> rejected) {
-                          return new Container(
-                            width: 200.0,
-                            height: 380.0,
-                            decoration: new BoxDecoration(
-                              shape: BoxShape.rectangle,
-                              color: Colors.purple,
-                              borderRadius: new BorderRadius.all(
-                                  new Radius.circular(20.0)),
-                            ),
-                            margin: const EdgeInsets.only(left: 0.0),
-                            child: Center(
-                                child: new ListView.builder(
-                              controller: widget.scrollControllerForGroupB,
-                              itemCount: widget.optionsOfGroupB?.length,
-                              itemBuilder: (context, i) => new Container(
-                                    margin: const EdgeInsets.all(7.0),
-                                    decoration: new BoxDecoration(
-                                      border: new Border.all(
-                                          color: Colors.black, width: 3.0),
-                                      color: Colors.orange,
-                                      boxShadow: [
-                                        new BoxShadow(
-                                          color: const Color(0x44000000),
-                                          spreadRadius: 2.0,
-                                          offset: const Offset(0.0, 1.0),
-                                        )
-                                      ],
-                                      borderRadius:
-                                          new BorderRadius.circular(12.0),
-                                    ),
-                                    child: new ListTile(
-                                      title: new Text(widget.optionsOfGroupB[i],
-                                          textAlign: TextAlign.center),
-                                    ),
-                                  ),
-                            )),
-                          );
-                        },
-                      ),
-                    ],
-                  )
+                  new GroupUI(
+                      removeData: removeData,
+                      group: groupB,
+                      optionsOfGroup: optionsOfGroupB,
+                      scrollControllerForGroup: scrollControllerForGroupB,
+                      shuffledOptions: shuffledOptions),
                 ],
               ),
             ),
@@ -310,8 +168,7 @@ class GameUIState extends State<GameUI> {
                 child: GridView.count(
                   childAspectRatio: 2.0,
                   crossAxisCount: 2,
-                  children:
-                      new List.generate(widget.shuffledOptions.length, (i) {
+                  children: new List.generate(shuffledOptions.length, (i) {
                     return new Container(
                       margin: const EdgeInsets.all(8.0),
                       decoration: new BoxDecoration(
@@ -326,7 +183,7 @@ class GameUIState extends State<GameUI> {
                         ],
                         borderRadius: new BorderRadius.circular(12.0),
                       ),
-                      child: new DragBox(widget.shuffledOptions[i]),
+                      child: new DragBox(shuffledOptions[i]),
                     );
                   }),
                 ),
@@ -335,6 +192,94 @@ class GameUIState extends State<GameUI> {
           )
         ],
       ),
+    );
+  }
+}
+
+class GroupUI extends StatelessWidget {
+  const GroupUI({
+    Key key,
+    @required this.removeData,
+    @required this.group,
+    @required this.optionsOfGroup,
+    @required this.scrollControllerForGroup,
+    @required this.shuffledOptions,
+  }) : super(key: key);
+  final Function(String) removeData;
+  final String group;
+  final List<String> optionsOfGroup;
+  final ScrollController scrollControllerForGroup;
+  final List<String> shuffledOptions;
+  @override
+  Widget build(BuildContext context) {
+    return new Column(
+      children: <Widget>[
+        Center(
+          child: Text(
+            group,
+            style: TextStyle(
+              color: Colors.black,
+              decoration: TextDecoration.none,
+              fontSize: 20.0,
+            ),
+          ),
+        ),
+        new DragTarget(
+          onAccept: (String label) {
+            SchedulerBinding.instance.addPostFrameCallback((_) {
+              scrollControllerForGroup.animateTo(
+                scrollControllerForGroup.position.maxScrollExtent,
+                duration: const Duration(milliseconds: 300),
+                curve: Curves.easeOut,
+              );
+            });
+
+            if (optionsOfGroup.contains(label)) {
+              return;
+            } else {
+              optionsOfGroup.add(label);
+              removeData(label);
+            }
+          },
+          builder: (BuildContext context, List<dynamic> accepted,
+              List<dynamic> rejected) {
+            return new Container(
+              width: 200.0,
+              height: 380.0,
+              decoration: new BoxDecoration(
+                shape: BoxShape.rectangle,
+                color: group == 'GroupA' ? Colors.blue : Colors.purple,
+                borderRadius: new BorderRadius.all(new Radius.circular(20.0)),
+              ),
+              margin: const EdgeInsets.only(left: 0.0),
+              child: Center(
+                  child: new ListView.builder(
+                controller: scrollControllerForGroup,
+                itemCount: optionsOfGroup?.length,
+                itemBuilder: (context, i) => new Container(
+                      margin: const EdgeInsets.all(7.0),
+                      decoration: new BoxDecoration(
+                        border: new Border.all(color: Colors.black, width: 3.0),
+                        color: Colors.orange,
+                        boxShadow: [
+                          new BoxShadow(
+                            color: const Color(0x44000000),
+                            spreadRadius: 2.0,
+                            offset: const Offset(0.0, 1.0),
+                          )
+                        ],
+                        borderRadius: new BorderRadius.circular(12.0),
+                      ),
+                      child: new ListTile(
+                        title: new Text(optionsOfGroup[i],
+                            textAlign: TextAlign.center),
+                      ),
+                    ),
+              )),
+            );
+          },
+        ),
+      ],
     );
   }
 }
