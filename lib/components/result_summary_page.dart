000e1731911fd5ce8c5016ef2527dc328112711a
sutara-aayush <35102016+sutara-aayush@users.noreply.github.com>
2018-08-21 12:41:08 +0530

    Added a simple layout structure for result summary page and did the requested changes.

--- lib/components/result_summary_page.dart
+++ lib/components/result_summary_page.dart
@@ -8,20 +8,122 @@ class ResultSummaryPage extends StatefulWidget {
 class _ResultSummaryPageState extends State<ResultSummaryPage> {
   @override
   Widget build(BuildContext context) {
-    return new LayoutBuilder(
-      builder: (context, constraints) {
-        return new Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            new Expanded(flex: 1, child: new Row()),
-            new Expanded(flex: 1, child: new Row()),
-            new Expanded(flex: 1, child: new Row()),
-            new Expanded(flex: 1, child: new Row()),
-            new Expanded(flex: 1, child: new Row()),
-          ],
-        );
-      },
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Result Summary Page"),
+        centerTitle: true,
+      ),
+      body: new Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          new Expanded(
+              flex: 1,
+              child: new Row(
+                children: <Widget>[
+                  new Expanded(
+                    flex: 1,
+                    child:
+                        new Container(child: new Center(child: new Text("1"))),
+                  ),
+                  new Expanded(
+                    flex: 3,
+                    child: new Container(
+                        child: new Center(child: new Text("Questions1"))),
+                  ),
+                  new Expanded(
+                    flex: 1,
+                    child: new Container(
+                        child: new Center(child: new Text("Score1"))),
+                  ),
+                ],
+              )),
+          new Expanded(
+              flex: 1,
+              child: new Row(
+                children: <Widget>[
+                  new Expanded(
+                    flex: 1,
+                    child:
+                        new Container(child: new Center(child: new Text("2"))),
+                  ),
+                  new Expanded(
+                    flex: 3,
+                    child: new Container(
+                        child: new Center(child: new Text("Questions2"))),
+                  ),
+                  new Expanded(
+                    flex: 1,
+                    child: new Container(
+                        child: new Center(child: new Text("Score2"))),
+                  ),
+                ],
+              )),
+          new Expanded(
+              flex: 1,
+              child: new Row(
+                children: <Widget>[
+                  new Expanded(
+                    flex: 1,
+                    child:
+                        new Container(child: new Center(child: new Text("3"))),
+                  ),
+                  new Expanded(
+                    flex: 3,
+                    child: new Container(
+                        child: new Center(child: new Text("Questions3"))),
+                  ),
+                  new Expanded(
+                    flex: 1,
+                    child: new Container(
+                        child: new Center(child: new Text("Score3"))),
+                  ),
+                ],
+              )),
+          new Expanded(
+              flex: 1,
+              child: new Row(
+                children: <Widget>[
+                  new Expanded(
+                    flex: 1,
+                    child:
+                        new Container(child: new Center(child: new Text("4"))),
+                  ),
+                  new Expanded(
+                    flex: 3,
+                    child: new Container(
+                        child: new Center(child: new Text("Questions4"))),
+                  ),
+                  new Expanded(
+                    flex: 1,
+                    child: new Container(
+                        child: new Center(child: new Text("Score4"))),
+                  ),
+                ],
+              )),
+          new Expanded(
+              flex: 1,
+              child: new Row(
+                children: <Widget>[
+                  new Expanded(
+                    flex: 1,
+                    child:
+                        new Container(child: new Center(child: new Text("5"))),
+                  ),
+                  new Expanded(
+                    flex: 3,
+                    child: new Container(
+                        child: new Center(child: new Text("Questions5"))),
+                  ),
+                  new Expanded(
+                    flex: 1,
+                    child: new Container(
+                        child: new Center(child: new Text("Score5"))),
+                  ),
+                ],
+              )),
+        ],
+      ),
     );
   }
 }
