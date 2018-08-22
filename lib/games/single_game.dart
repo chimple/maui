a3efa937779e2a27e673c15a93cc8d977dc1cc3c
Rajesh-Chimple <35517566+Rajesh-Chimple@users.noreply.github.com>
2018-08-22 11:31:45 +0530

    #773 Made Requested Changes

--- lib/games/single_game.dart
+++ lib/games/single_game.dart
@@ -1,9 +1,8 @@
 import 'dart:convert';
 import 'dart:math';
 import 'dart:async';
-
 import 'package:flutter/cupertino.dart';
-import 'package:maui/components/topic_page_view.dart';
+import 'package:maui/components/show_help.dart';
 import 'package:maui/screens/topic_screen.dart';
 import 'package:uuid/uuid.dart';
 import 'package:flutter/material.dart';
@@ -1128,23 +1127,3 @@ class _SingleGameState extends State<SingleGame> with TickerProviderStateMixin {
   }
 }
 
-class ShowHelpDrawer extends StatelessWidget {
- final String topicId;
-  const ShowHelpDrawer({
-    Key key,
-    @required this.topicId,
-  }) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return new LayoutBuilder(builder: (context, constraints) {
-      print("Size ${constraints.maxHeight} , ${constraints.maxWidth}");
-      return new Container(
-        height: constraints.maxHeight,
-        width: constraints.maxWidth * 0.89,
-        child: new Drawer(
-          child: new TopicPageView(topicId: topicId,),
-        ),
-      );
-    });
-  }
-}
