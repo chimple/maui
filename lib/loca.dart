e89a4ea8e3cb1a18515b0ecf95a005b490aad7a3
shantkumar <shant@sutara.org>
2018-09-03 15:50:16 +0530

    Internationalize and removed MaterialApp

--- lib/loca.dart
+++ lib/loca.dart
@@ -22,6 +22,8 @@ class Loca {
   String intl(String name) => Intl.message(name, name: name);
   String get title => Intl.message('Maui', name: 'title');
   String get chat => Intl.message('Chat', name: 'chat');
+   String get gallery => Intl.message('Gallery', name: 'gallery');
+    String get topic => Intl.message('Topic', name: 'topic');
   String get game => Intl.message('Game', name: 'game');
   String get category => Intl.message('Category', name: 'category');
   String get profile => Intl.message('Profile', name: 'profile');
--- lib/screens/profile_view.dart
+++ lib/screens/profile_view.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import '../db/entity/quiz.dart';
+import '../loca.dart';
 import '../repos/quiz_repo.dart';
 import '../repos/topic_repo.dart';
 import 'package:flutter/material.dart';
@@ -12,11 +13,19 @@ class ProfileView extends StatefulWidget {
   ProfileViewState createState() => ProfileViewState();
 }
 
-class ProfileViewState extends State<ProfileView> {
+class ProfileViewState extends State<ProfileView> with TickerProviderStateMixin {
   List<String> categories = [
-    "Gallery",
-    "Topic",
+    "gallery",
+    "topic",
   ];
+  TabController _controller;
+
+   @override
+  void initState() {
+    super.initState();
+    print("Welcome to QuizProgressTracker class");
+    _controller = new TabController(length: 4, vsync: this);
+  }
 
   @override
   Widget build(BuildContext ctxt) {
@@ -24,78 +33,76 @@ class ProfileViewState extends State<ProfileView> {
     Orientation orientation = media.orientation;
     var _size = media.size;
     var user = AppStateContainer.of(context).state.loggedInUser;
-    return new MaterialApp(
-        home: DefaultTabController(
-            length: categories.length,
-            child: new Scaffold(
-              body: new NestedScrollView(
-                // controller: _scrollcontroller,
-                headerSliverBuilder:
-                    (BuildContext context, bool innerBoxIsScrolled) {
-                  return <Widget>[
-                    new SliverAppBar(
-                      backgroundColor: const Color(0xffFC5E79),
-                      pinned: true,
+    return  new Scaffold(
+          body: new NestedScrollView(
+            // controller: _scrollcontroller,
+            headerSliverBuilder:
+                (BuildContext context, bool innerBoxIsScrolled) {
+              return <Widget>[
+                new SliverAppBar(
+                  backgroundColor: const Color(0xffFC5E79),
+                  pinned: true,
 
-                      expandedHeight: orientation == Orientation.portrait
-                          ? _size.height * .25
-                          : _size.height * .5,
-                      // forceElevated:false,
-                      flexibleSpace: new FlexibleSpaceBar(
-                        background: new FittedBox(
-                          fit: BoxFit.contain,
-                          alignment: Alignment.center,
-                          child: Container(
-                            height: 300.0,
-                            width: 300.0,
-                            child: new Center(
-                                child: Column(children: [
-                              new Container(
-                                height: 200.0,
-                                width: 200.0,
-                                decoration: new BoxDecoration(
-                                  borderRadius: const BorderRadius.all(
-                                      const Radius.circular(16.0)),
-                                ),
-                                child: Image.file(new File(user.image)),
-                              ),
-                              new Text("${user.name}")
-                            ])),
+                  expandedHeight: orientation == Orientation.portrait
+                      ? _size.height * .25
+                      : _size.height * .5,
+                  // forceElevated:false,
+                  flexibleSpace: new FlexibleSpaceBar(
+                    background: new FittedBox(
+                      fit: BoxFit.contain,
+                      alignment: Alignment.center,
+                      child: Container(
+                        height: 300.0,
+                        width: 300.0,
+                        child: new Center(
+                            child: Column(children: [
+                          new Container(
+                            height: 200.0,
+                            width: 200.0,
+                            decoration: new BoxDecoration(
+                              borderRadius: const BorderRadius.all(
+                                  const Radius.circular(16.0)),
+                            ),
+                            child: Image.file(new File(user.image)),
                           ),
-                        ),
-                      ),
-                      bottom: new TabBar(
-                        isScrollable: false,
-                        indicatorColor: Colors.white,
-                        indicatorSize: TabBarIndicatorSize.tab,
-                        indicatorWeight: 5.0,
-                        labelColor: Colors.white,
-                        labelStyle: new TextStyle(
-                            fontSize: _size.height * 0.3 * 0.07,
-                            fontWeight: FontWeight.bold,
-                            fontStyle: FontStyle.normal),
-                        // controller: _controller,
-                        // unselectedLabelColor: _myHandler.color,
-                        tabs: <Tab>[
-                          new Tab(
-                            text: "Gallery",
-                          ),
-                          new Tab(
-                            text: "Topic",
-                          ),
-                        ],
+                          new Text("${user.name}")
+                        ])),
                       ),
                     ),
-                  ];
-                },
-                body: new TabBarView(
-                  // controller: _controller,
-                  children: <Widget>[
-                    new Text("i have to show gallery here"),
-                    new TopicView(),
-                  ],
+                  ),
+                  bottom: new TabBar(
+                    isScrollable: false,
+                    indicatorColor: Colors.white,
+                    indicatorSize: TabBarIndicatorSize.tab,
+                    indicatorWeight: 5.0,
+                    labelColor: Colors.white,
+                    labelStyle: new TextStyle(
+                        fontSize: _size.height * 0.3 * 0.07,
+                        fontWeight: FontWeight.bold,
+                        fontStyle: FontStyle.normal),
+                    controller: _controller,
+                    // unselectedLabelColor: _myHandler.color,
+                    tabs: <Tab>[
+                   
+                         new Tab(
+                  text: Loca.of(context).gallery,
                 ),
-              ),
-            )));
+                new Tab(
+                  text: Loca.of(context).topic,
+                ),
+                    ],
+                  ),
+                ),
+              ];
+            },
+            body: new TabBarView(
+              controller: _controller,
+              children: <Widget>[
+                new Text("i have to show gallery here"),
+                new TopicView(),
+              ],
+            ),
+          ),
+        );
   }
 }
