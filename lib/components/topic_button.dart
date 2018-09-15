55f60c49c80531e74e59e7f05e34889a09432ba5
sutara-aayush <35102016+sutara-aayush@users.noreply.github.com>
2018-09-15 14:55:09 +0530

    Topic button, Unnecessary use of widget removed.

--- lib/components/topic_button.dart
+++ lib/components/topic_button.dart
@@ -39,54 +39,60 @@ class TopicButtonState extends State<TopicButton> {
       padding: const EdgeInsets.all(8.0),
       child: new Material(
         elevation: 8.0,
+        color: Colors.brown,
         borderRadius: const BorderRadius.all(const Radius.circular(16.0)),
-        child: new Container(
-          color: Colors.brown,
-          child: new InkWell(
-            onTap: widget.onPress,
-            child: new Stack(children: [
-              new Column(
-                children: <Widget>[
-                  new QuizProgressTracker(topicId: widget.topicId),
-                  widget.image == null
+        child: new InkWell(
+          onTap: widget.onPress,
+          child: new Column(
+            children: <Widget>[
+              new Expanded(
+                  flex: 1,
+                  child: new QuizProgressTracker(topicId: widget.topicId)),
+              widget.image == null
+                  ? new Expanded(
+                      flex: 12,
+                      child: Container(color: Colors.red),
+                    )
+                  : widget.image.endsWith(".svg")
                       ? new Expanded(
-                          child: Container(color: Colors.red),
-                        )
-                      : widget.image.endsWith(".svg")
-                          ? new Expanded(
-                              child: new Container(
-                                color: Colors.red,
-                                child: new AspectRatio(
-                                  aspectRatio: 2.0,
-                                  child: new SvgPicture.asset(
-                                    widget.image,
-                                    allowDrawingOutsideViewBox: false,
-                                  ),
-                                ),
-                              ),
-                            )
-                          : new Expanded(
-                              child: Container(
-                                decoration: new BoxDecoration(
-                                    image: new DecorationImage(
-                                      image: new AssetImage(widget.image),
-                                      fit: BoxFit.cover,
-                                    ),
-                                    color: Colors.red),
+                          flex: 12,
+                          child: new Container(
+                            color: Colors.red,
+                            child: new AspectRatio(
+                              aspectRatio: 2.0,
+                              child: new SvgPicture.asset(
+                                widget.image,
+                                allowDrawingOutsideViewBox: false,
                               ),
                             ),
-                  Container(
+                          ),
+                        )
+                      : new Expanded(
+                          flex: 12,
+                          child: Container(
+                            decoration: new BoxDecoration(
+                                image: new DecorationImage(
+                                  image: new AssetImage(widget.image),
+                                  fit: BoxFit.cover,
+                                ),
+                                color: Colors.red),
+                          ),
+                        ),
+              new Expanded(
+                flex: 1,
+                child: new Container(
+                  child: new Center(
                     child: new Text(widget.text,
                         style: new TextStyle(
                           color: Colors.white,
                           fontSize: 30.0,
                         ),
-//                        textAlign: TextAlign.center,
+                        textAlign: TextAlign.center,
                         overflow: TextOverflow.ellipsis),
-                  )
-                ],
+                  ),
+                ),
               )
-            ]),
+            ],
           ),
         ),
       ),
