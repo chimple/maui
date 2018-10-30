870b0552b6d66338c0fc53f961e74d05d1908d17
Srikanth Talapadi <sri@zoolore.org>
2018-10-30 17:11:04 +0530

    added african story book support

---
+++ scripts/refiloe.html.xlsx.sql
@@ -0,0 +1,64 @@
+INSERT INTO `card` (id, type, title, header, content, option) VALUES ('refiloe', 2, 'Refiloe and the washed chickens', 'assets/topic/asb/986.png', 'Marion Drew
+Wiehan de Jager', NULL);
+INSERT INTO `card` (id, type, title, header, content, option) VALUES ('refiloe3', 3, NULL, 'assets/topic/asb/1003.png', 'It was the first wedding in the Tenane family, and Refiloe had never been so excited in her life. "I''m going to be Palesa''s bridesmaid, with a new dress and shoes!" she said to her chickens. Refiloe told her chickens everything. Her father had
+          given the chickens to her as a birthday present, and she loved them very much.', NULL);
+INSERT INTO `card` (id, type, title, header, content, option) VALUES ('refiloe4', 3, NULL, 'assets/topic/asb/1011.png', '"Take those dirty scruffy chickens out of here Refiloe!" shouted her mother, "I don''t want them anywhere near this wedding tent."', NULL);
+INSERT INTO `card` (id, type, title, header, content, option) VALUES ('refiloe5', 3, NULL, 'assets/topic/asb/989.png', 'Refiloe shooed her chickens out of the tent. "Run away you silly things." she said, "I don''t want you to be eaten at Palesa''s wedding!"', NULL);
+INSERT INTO `card` (id, type, title, header, content, option) VALUES ('refiloe6', 3, NULL, 'assets/topic/asb/1013.png', '"Mme says that this is going to be the best wedding ever seen in Malealea," Refiloe told her chickens, "and she says that I can help all the older girls with the cooking." The chickens paid no attention to her. "Refiloe!" called her mother from
+          inside, "leave those chickens of yours and come and help me with these pots please!"', NULL);
+INSERT INTO `card` (id, type, title, header, content, option) VALUES ('refiloe7', 3, NULL, 'assets/topic/asb/992.png', NULL, NULL);
+INSERT INTO `card` (id, type, title, header, content, option) VALUES ('refiloe8', 3, NULL, 'asb/', 'Refiloe''s mother was very proud of her house, and wanted it to be perfect for the wedding. She cleaned and washed and scrubbed and scoured and polished everything in sight. When she had finished, there was not a single speck of dust anywhere.
+          The chickens poked their heads inside the front door. "Out!" shouted Refiloe''s mother, "out you scruffy things with your dirty feet and beaks!"', NULL);
+INSERT INTO `card` (id, type, title, header, content, option) VALUES ('refiloe9', 3, NULL, 'assets/topic/asb/1018.png', 'Refiloe followed her chickens out into the yard. "Mme is right, you are a disgrace," she scolded them So Refiloe thought for a while. Suddenly she smiled and said to herself, "I have an excellent idea." She went and filled a bucket with water
+          from the spring. Refiloe had a lot of trouble catching her chickens. They did not think that a bath was such a good idea.', NULL);
+INSERT INTO `card` (id, type, title, header, content, option) VALUES ('refiloe10', 3, NULL, 'assets/topic/asb/993.png', 'Refiloe put the first chicken into the bucket of water. It clucked at the top of its voice and flapped its wings like crazy. "Hold still you silly thing," Refiloe shouted, "this won''t take long!" When Refiloe wiped the chicken''s nose and eyes
+          it suddenly went limp and flopped over the side of the bucket.', NULL);
+INSERT INTO `card` (id, type, title, header, content, option) VALUES ('refiloe11', 3, NULL, 'assets/topic/asb/1000.png', '"This is no time to sleep," said Refiloe. She gave it a good shake to get all the water out. "If you are not going to wake up now, I''ll have to put you somewhere to dry nicely," she said. She laid the chicken out on the grass to dry. The chicken
+          lay completely still.', NULL);
+INSERT INTO `card` (id, type, title, header, content, option) VALUES ('refiloe12', 3, NULL, 'assets/topic/asb/994.png', 'Refiloe washed each and every one of her chickens. Each and every one of them went limp and floppy before she was finished with it. She put all those eight chickens in a row on the grass to dry. Not one of them moved a single feather. "I''ll leave
+          them to sleep a little," Refiloe thought to herself, and off she went to check on the moroho.', NULL);
+INSERT INTO `card` (id, type, title, header, content, option) VALUES ('refiloe13', 3, NULL, 'assets/topic/asb/990.png', 'Mme Ngwe and Mme Pedi were Refiloe''s aunts. They did not like each other at all. The aunts had been preparing for months for Palesa''s wedding, but they would not even think about riding together in the same taxi.', NULL);
+INSERT INTO `card` (id, type, title, header, content, option) VALUES ('refiloe14', 3, NULL, 'assets/topic/asb/995.png', 'Mme Ngwe spotted Refiloe''s chickens laid out to dry. "What a nice little snack for me to take home!" she said to herself. She took off her chale and carefully wrapped all those chickens up in it. Not a single chicken moved. "Perfect!" she smiled,
+          "now I shall put them somewhere where Mme Pedi will not ever find them," and she put the bundle around the corner among the pumpkin vines.', NULL);
+INSERT INTO `card` (id, type, title, header, content, option) VALUES ('refiloe15', 3, NULL, 'asb/', 'Mme Pedi decided to make her famous pumpkin dish. She took her big bowl and went off to the vegetable garden. In the vegetable garden she saw Mme Ngwe''s bundle under the pumpkin vine. She undid the bundle. Out fell all eight chickens. "Eish!"
+          she cried jumping back in surprise. "Oh, you are the cleanest, prettiest chickens! Just ready for my pot," she crooned. "Now, where shall I hide you my darlings?" Mme Pedi chuckled, "I''ll have to find a very good place," she said walking round
+          the house. "I know," she said at last, "I''ll put you on the roof!" Mme Pedi climbed up onto the garden wall and put the chickens in a row on the thatched roof.', NULL);
+INSERT INTO `card` (id, type, title, header, content, option) VALUES ('refiloe16', 3, NULL, 'assets/topic/asb/997.png', NULL, NULL);
+INSERT INTO `card` (id, type, title, header, content, option) VALUES ('refiloe17', 3, NULL, 'asb/', 'The next day the sun rose in good time for the wedding. Refiloe was up first and went to see her chickens. She saw that they were no longer lying on the grass. "They must be nicely dry by now. I''m sure they have gone to look for their breakfast,"
+          she said.', NULL);
+INSERT INTO `card` (id, type, title, header, content, option) VALUES ('refiloe18', 3, NULL, 'assets/topic/asb/1047.png', 'Palesa''s wedding was wonderful. Those chickens did not get up off the roof to join in when all the bridesmaid ladies danced into the yard. Not one of the chickens stirred when the church choir sang in their loudest, sweetest voices. The chickens
+          were still lying on the roof when the priest gave his sermon. They did not even twitch a feather when the pigs and Refiloe''s grandfather all snored loudly in the middle of the sermon. Not one foot of those chickens trembled when sheep wandered
+          into the tent, nearly knocking over the wedding cake.', NULL);
+INSERT INTO `card` (id, type, title, header, content, option) VALUES ('refiloe19', 3, NULL, 'assets/topic/asb/999.png', 'It was only when the groom''s father was halfway through his speech that things started to change on that roof.', NULL);
+INSERT INTO `card` (id, type, title, header, content, option) VALUES ('refiloe20', 3, NULL, 'asb/', 'The first chicken spread its wings and flew onto Mme Ngwe''s bosom. Mme Pedi next to her started giggling. Another chicken flew up onto Mme Ngwe''s new duku. The people at the next table tried not to laugh. The lady next to Mme Ngwe put her head
+          down onto the table, "Ai, aai, aaaaii, he, he, heeeeeeee!" she cackled. All the other chickens decided to join the first two. You couldn''t see Mme Ngwe for all those chickens!', NULL);
+INSERT INTO `card` (id, type, title, header, content, option) VALUES ('refiloe21', 3, NULL, 'assets/topic/asb/1002.png', NULL, NULL);
+INSERT INTO `card` (id, type, title, header, content, option) VALUES ('refiloe22', 3, NULL, 'asb/', 'The guests screamed with laughter. The men had to hold onto their bellies. The women rolled around in their chairs. The boys and girls were hanging onto each other. The grandmothers could hardly breathe for laughing. The grandfathers stamped their
+          walking sticks. Then the two aunts looked at each other and started giggling. Mme Pedi opened her mouth wide with laughter. Mme Ngwe threw back her head and laughed until all her chins shook. Refiloe couldn''t believe it!', NULL);
+INSERT INTO `card` (id, type, title, header, content, option) VALUES ('refiloe23', 3, NULL, 'assets/topic/asb/1046.png', NULL, NULL);
+INSERT INTO `card` (id, type, title, header, content, option) VALUES ('refiloe24', 3, NULL, 'asb/', 'All the guests agreed that it was the best wedding entertainment ever known in Malealea. "Oh you are so lucky!" Refiloe said to her chickens as she put them into their coop for the night, "Mme says that she will never put you in the supper pot."
+          "But look how dirty you are again," she told them, "I think I''ll have to give you another wash tomorrow!"', NULL);
+INSERT INTO `card` (id, type, title, header, content, option) VALUES ('refiloe25', 3, NULL, 'assets/topic/asb/1048.png', NULL, NULL);
+INSERT INTO `collection` (id, serial, cardId) VALUES ('refiloe', 1, 'refiloe3');
+INSERT INTO `collection` (id, serial, cardId) VALUES ('refiloe', 2, 'refiloe4');
+INSERT INTO `collection` (id, serial, cardId) VALUES ('refiloe', 3, 'refiloe5');
+INSERT INTO `collection` (id, serial, cardId) VALUES ('refiloe', 4, 'refiloe6');
+INSERT INTO `collection` (id, serial, cardId) VALUES ('refiloe', 5, 'refiloe7');
+INSERT INTO `collection` (id, serial, cardId) VALUES ('refiloe', 6, 'refiloe8');
+INSERT INTO `collection` (id, serial, cardId) VALUES ('refiloe', 7, 'refiloe9');
+INSERT INTO `collection` (id, serial, cardId) VALUES ('refiloe', 8, 'refiloe10');
+INSERT INTO `collection` (id, serial, cardId) VALUES ('refiloe', 9, 'refiloe11');
+INSERT INTO `collection` (id, serial, cardId) VALUES ('refiloe', 10, 'refiloe12');
+INSERT INTO `collection` (id, serial, cardId) VALUES ('refiloe', 11, 'refiloe13');
+INSERT INTO `collection` (id, serial, cardId) VALUES ('refiloe', 12, 'refiloe14');
+INSERT INTO `collection` (id, serial, cardId) VALUES ('refiloe', 13, 'refiloe15');
+INSERT INTO `collection` (id, serial, cardId) VALUES ('refiloe', 14, 'refiloe16');
+INSERT INTO `collection` (id, serial, cardId) VALUES ('refiloe', 15, 'refiloe17');
+INSERT INTO `collection` (id, serial, cardId) VALUES ('refiloe', 16, 'refiloe18');
+INSERT INTO `collection` (id, serial, cardId) VALUES ('refiloe', 17, 'refiloe19');
+INSERT INTO `collection` (id, serial, cardId) VALUES ('refiloe', 18, 'refiloe20');
+INSERT INTO `collection` (id, serial, cardId) VALUES ('refiloe', 19, 'refiloe21');
+INSERT INTO `collection` (id, serial, cardId) VALUES ('refiloe', 20, 'refiloe22');
+INSERT INTO `collection` (id, serial, cardId) VALUES ('refiloe', 21, 'refiloe23');
+INSERT INTO `collection` (id, serial, cardId) VALUES ('refiloe', 22, 'refiloe24');
+INSERT INTO `collection` (id, serial, cardId) VALUES ('refiloe', 23, 'refiloe25');
