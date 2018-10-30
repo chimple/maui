f9dfb8a72bd94ae55c3cc9f956577a48e56a471a
shantkumar <shant@sutara.org>
2018-10-30 16:04:07 +0530

    Cardlock popUp screen

--- lib/quack/card_lock.dart
+++ lib/quack/card_lock.dart
@@ -8,19 +8,26 @@ import 'package:maui/models/root_state.dart';
 import 'package:maui/quack/card_detail.dart';
 import 'package:maui/quack/collection_progress_indicator.dart';
 import 'package:maui/repos/card_progress_repo.dart';
+import 'package:maui/repos/user_repo.dart';
+import '../actions/deduct_points.dart';
 
 class CardLock extends StatelessWidget {
   final QuackCard card;
   final String parentCardId;
 
-  const CardLock({Key key, this.card, this.parentCardId}) : super(key: key);
-
+  CardLock({Key key, this.card, this.parentCardId}) : super(key: key);
+  var userPoints;
   @override
   Widget build(BuildContext context) {
     return Connect<RootState, double>(
-      convert: (state) => state.progressMap[card.id],
+      convert: (state) {
+        userPoints = state.user.points;
+        return state.progressMap[card.id];
+      },
       where: (prev, next) => next != prev,
       builder: (progress) {
+        // userPoints=state.user.points;
+        print("hello data is.....${userPoints}");
         return Padding(
           padding: const EdgeInsets.all(8.0),
           child: progress == null
@@ -50,6 +57,7 @@ class CardLock extends StatelessWidget {
     Provider.dispatch<RootState>(context, FetchCardDetail(card.id));
     Provider.dispatch<RootState>(
         context, AddProgress(card: card, parentCardId: card.id));
+
     Navigator.of(context).push(
       MaterialPageRoute(
           builder: (BuildContext context) => CardDetail(
@@ -64,19 +72,67 @@ class CardLock extends StatelessWidget {
             context: context,
             builder: (BuildContext context) {
               return SimpleDialog(
-                title: const Text('Unlock?'),
+                titlePadding: EdgeInsets.all(0.0),
+                title: Container(
+                    height: 60.0,
+                    //             decoration: new BoxDecoration(
+                    // shape: BoxShape.rectangle,
+                    // // color: const Color(0xFFFFFF),
+                    // borderRadius:
+                    //     new BorderRadius.all(new Radius.circular(32.0)),
+                    // ),
+                    color: Colors.blue,
+                    child: Center(child: Text('your points-$userPoints'))),
                 children: <Widget>[
-                  SimpleDialogOption(
-                    onPressed: () {
-                      Navigator.pop(context, true);
-                    },
-                    child: const Text('Yes'),
-                  ),
-                  SimpleDialogOption(
-                    onPressed: () {
-                      Navigator.pop(context, false);
-                    },
-                    child: const Text('No'),
+                  // SimpleDialogOption(
+                  //   onPressed: () {
+                  //     Navigator.pop(context, true);
+                  //   },
+                  //   child: const Text('Yes'),
+                  // ),
+                  // SimpleDialogOption(
+                  //   onPressed: () {
+                  //     Navigator.pop(context, false);
+                  //   },
+                  //   child: const Text('No'),
+                  // ),
+                  new Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                          width: 200.0,
+                          height: 200.0,
+                          child: Card(
+                            child: new Image.asset(
+                              'assets/Fruits.png',
+                              fit: BoxFit.cover,
+                            ),
+                          )),
+                      Container(
+                        height: 200.0,
+                        width: 200.0,
+                        child: Card(
+                          child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                new Text(
+                                  "cost- 1",
+                                ),
+                                new RaisedButton(
+                                  onPressed: userPoints >= 5
+                                      ? () {
+                                          // new DeductPoints(points: 1);
+                                          Provider.dispatch<RootState>(
+                                              context, DeductPoints(points: 1));
+                                          Navigator.pop(context, true);
+                                        }
+                                      : null,
+                                  child: Text("buy"),
+                                )
+                              ]),
+                        ),
+                      )
+                    ],
                   ),
                 ],
               );
