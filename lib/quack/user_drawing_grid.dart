0e6df110484d4e19e9211cfe0aee4c448eccd832
Srikanth Talapadi <sri@zoolore.org>
2018-11-13 15:33:32 +0530

    Added collection fixes #825

--- lib/quack/user_drawing_grid.dart
+++ lib/quack/user_drawing_grid.dart
@@ -5,26 +5,55 @@ import 'package:maui/models/root_state.dart';
 import 'package:maui/quack/drawing_card.dart';
 import 'package:maui/repos/tile_repo.dart';
 
-class UserDrawingGrid extends StatelessWidget {
+class UserDrawingGrid extends StatefulWidget {
+  final String userId;
+
+  const UserDrawingGrid({Key key, this.userId}) : super(key: key);
+
+  @override
+  UserDrawingGridState createState() {
+    return new UserDrawingGridState();
+  }
+}
+
+class UserDrawingGridState extends State<UserDrawingGrid> {
+  bool _isLoading = true;
+  List<Tile> _tiles;
+
+  @override
+  void initState() {
+    super.initState();
+    _initData();
+  }
+
+  void _initData() async {
+    _tiles = await TileRepo()
+        .getTilesByUserIdAndType(widget.userId, TileType.drawing);
+    print('tiles:$_tiles');
+    setState(() => _isLoading = false);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Connect<RootState, List<Tile>>(
-      convert: (state) => state.tiles,
-      where: (prev, next) => next != prev,
-      builder: (drawings) {
-        return GridView.builder(
-          gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 3,
-          ),
-          itemBuilder: (BuildContext context, int index) {
-            return Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: DrawingCard(tile: drawings[index]),
-            );
-          },
-          itemCount: drawings.length,
+    if (_isLoading) {
+      return Center(
+          child: new SizedBox(
+        width: 20.0,
+        height: 20.0,
+        child: new CircularProgressIndicator(),
+      ));
+    }
+    return GridView.builder(
+      gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 3,
+      ),
+      itemBuilder: (BuildContext context, int index) {
+        return Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: DrawingCard(tile: _tiles[index]),
         );
       },
+      itemCount: _tiles.length,
     );
   }
 }
