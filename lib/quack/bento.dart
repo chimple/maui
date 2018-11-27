86d286ed61db3b9a7a9d7c0576df1c6c07c6384c
kiran kumar bv <kiran.bv@zoolore.org>
2018-11-27 23:27:29 +0530

    Added Three Floating action button

--- lib/quack/bento.dart
+++ lib/quack/bento.dart
@@ -7,6 +7,7 @@ import 'package:maui/db/entity/quack_card.dart';
 import 'package:maui/home/home_screen.dart';
 import 'package:maui/models/root_state.dart';
 import 'package:maui/quack/card_summary.dart';
+import 'package:maui/quack/fab_icon.dart';
 import 'package:maui/quack/friend_strip.dart';
 import 'package:maui/quack/post_comments.dart';
 
@@ -24,38 +25,7 @@ class Bento extends StatelessWidget {
       ),
       child: Scaffold(
         backgroundColor: Color(0xffeeeeee),
-        floatingActionButton: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: FloatingActionButton(
-                  heroTag: "draw",
-                  child: Icon(Icons.edit),
-                  onPressed: () {
-                    print("hiiiiiiiiiiiiii");
-                    Navigator.of(context).push(
-                      new MaterialPageRoute(
-                          builder: (BuildContext context) =>
-                              DrawingWrapper(activityId: "dummy")),
-                    );
-                  }),
-            ),
-            Padding(padding: EdgeInsets.all(2.0),),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: FloatingActionButton(
-                  heroTag: "post",
-                  child: Icon(Icons.comment),
-                  onPressed: () {
-                    Navigator.of(context).push(
-                      new MaterialPageRoute(
-                          builder: (BuildContext context) => PostComments()),
-                    );
-                  }),
-            ),
-          ],
-        ),
+        floatingActionButton: FancyFab(),
         body: Column(
           children: <Widget>[
             FriendStrip(),
