2c41e555f17f461f6f598513e3347305d3d3c104
Rishabh Singh <36836361+rishabh-chimple@users.noreply.github.com>
2019-01-18 14:27:08 +0530

    Code Updated For Sequence number and Widget Test

--- test/sequence_number_widget_test.dart
+++ test/sequence_number_widget_test.dart
@@ -3,9 +3,23 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:maui/games/sequence_the_number.dart';
 
 void main() {
-  testWidgets('Widget testing for Matching', (WidgetTester tester) async {
+  testWidgets('Sequence the number', (WidgetTester tester) async {
+    print('Sequence the Numbr Game Test');
+    var testKey = UniqueKey();
     await tester.pumpWidget(new MaterialApp(
-      home: SequenceTheNumber(),
+      home: new Material(
+        child: new SequenceTheNumber(
+          // input: testMap,
+          key: testKey,
+        ),
+      ),
     ));
+    expect(find.byKey(testKey), findsOneWidget);
+    await tester.pump(Duration(seconds: 5));
+    final Offset firstLocation = tester.getCenter(find.byType(MyButton).first);
+    await tester.tap(find.byType(MyButton).first);
+    await tester.pump(Duration(seconds: 5));
+    await tester.fling(find.byType(MyButton).last, firstLocation, 8.0);
+    await tester.pump(Duration(seconds: 5));
   });
 }
