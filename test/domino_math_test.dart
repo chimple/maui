032150a0c399f2748a9c87c66db07a596571b284
ramaranjandey <36834875+ramaranjandey@users.noreply.github.com>
2019-01-18 16:20:03 +0530

    Update domino_math_test.dart

--- test/domino_math_test.dart
+++ test/domino_math_test.dart
@@ -5,7 +5,6 @@ import 'package:maui/games/domino_math.dart';
 void main() {
   testWidgets('Domino math', (WidgetTester tester) async {
     Key inputKey = new UniqueKey();
-    
 
     await tester.pumpWidget(new MaterialApp(
       home: new Material(
@@ -14,9 +13,14 @@ void main() {
         ),
       ),
     ));
-  //  await tester.tap(find.byType(RaisedButton).last);
-     expect(find.byKey(inputKey), findsOneWidget);
+    //  await tester.tap(find.byType(RaisedButton).last);
+    expect(find.byKey(inputKey), findsOneWidget);
+    await tester.pump(Duration(seconds: 5));
+    final Offset firstLocation =
+        tester.getCenter(find.byType(DominoAnswer).first);
+    await tester.tap(find.byType(DominoAnswer).first);
+    await tester.pump(Duration(seconds: 5));
+    await tester.fling(find.byType(DominoAnswer).last, firstLocation, 8.0);
+    await tester.pump(Duration(seconds: 5));
   });
-
-  
 }
