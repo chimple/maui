6055d7bc2bf20f0709ce68339e21532e9ba25613
Manukumar S B <manu@sutara.org>
2019-02-07 15:02:49 +0530

    score screen updated

--- lib/components/move_animation.dart
+++ lib/components/move_animation.dart
@@ -1,10 +1,9 @@
 import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'package:flutter/scheduler.dart' show timeDilation;
 
 class MoveContainer extends StatefulWidget {
-  final int coinCount;
+   int coinCount;
   List<int> starValue;
   int index;
   Offset offset;
@@ -46,11 +45,10 @@ class _MyMoveContainer extends State<MoveContainer>
   var countOpacity = 1.0;
   var size = 0.0;
   var extraSize = 0.0;
-  double start;
   double ending;
-  MediaQueryData media;
   int index = 100;
   double animationDuration = 0.0;
+  bool animeDone = false;
 
   @override
   void initState() {
@@ -62,21 +60,14 @@ class _MyMoveContainer extends State<MoveContainer>
     // start = (widget.duration * widget.starCount) * 3.toDouble();
     // ending = (start + widget.duration) * 2500;
     // Offset endOffset = Offset(ending, 0.0);
-     final int totalDuration = 4000;
+    final int totalDuration = 2000;
     _controller = AnimationController(
         vsync: this, duration: new Duration(milliseconds: totalDuration));
-    animationDuration = totalDuration / (100 * (totalDuration / widget.starCount));
-    // _controller = AnimationController(
-    //   vsync: this,
-    //   duration: const Duration(seconds: 4),
-    // );
-    // print("start and end iss     $start     ,,,,, .... $end");
-    // _offset =
-    //    Tween<Offset>(begin: begin / 100, end: -(widget.offset) / 100)
-    //         .animate(_controller);
+    animationDuration = totalDuration / (100 * (totalDuration / widget.index));
+
     _width = Tween<double>(
       begin: 0,
-      end: -40,
+      end: 50,
     ).animate(
       CurvedAnimation(
         parent: _controller,
@@ -89,12 +80,33 @@ class _MyMoveContainer extends State<MoveContainer>
     );
 
     _width.addListener(() {
-    // setState(() {
-    extraSize = _width.value+ _controller.value+4;
-    // });
+      print(
+          "start issssssssssssssssssssssssssssssssssssssssssssssssssssssssssss  ${_controller.value}");
+      if (_controller.value < 0.3) {
+        extraSize = extraSize;
+      } else if (_controller.value > 0.3 && _controller.value < 0.35) {
+        extraSize = extraSize + 4;
+      } else if (_controller.value > 0.35 && _controller.value < 0.6) {
+        extraSize = extraSize + .5;
+      } else if (_controller.value > 0.6 && _controller.value < .7) {
+        extraSize = extraSize - 1.5;
+      } else {
+        extraSize = extraSize - 2.5;
+      }
+      // setState(() {
+      // extraSize = _controller.value > 0.3 ? _width.value + _controller.value + 4 : ;
+      // });
     });
     _controller.forward();
-    
+
+    _controller.addStatusListener((status) {
+      if (_controller.isCompleted) {
+        setState(() {
+          animeDone = true;
+           widget.coinCount = widget.starCount;
+        });
+      }
+    });
   }
 
   void _afterLayout() {
@@ -102,7 +114,7 @@ class _MyMoveContainer extends State<MoveContainer>
         _globalKey2.currentContext.findRenderObject();
     Offset offset = -renderBoxRed.globalToLocal(Offset.zero);
     begin = offset;
-    print("ofsetsssssssssssssssssssssssssssisss  $begin");
+    // print("ofsetsssssssssssssssssssssssssssisss  $begin");
   }
 
   @override
@@ -113,41 +125,50 @@ class _MyMoveContainer extends State<MoveContainer>
 
   @override
   Widget build(BuildContext context) {
-    start = (animationDuration * widget.index).toDouble();
+    double start = (animationDuration * widget.index).toDouble();
     // myEnd = (start + widget.duration).toDouble();
     Size media = MediaQuery.of(context).size;
-    return AnimatedBuilder(
-        animation: _controller,
-        builder: (BuildContext context, Widget child) {
-          return Stack(
-            children: <Widget>[
-              _Animated(
-                scale: Tween<Offset>(
-                        begin: begin / 100, end: (widget.offset) / 100)
-                    .animate(
-                  CurvedAnimation(
-                    parent: _controller,
-                    curve: Interval(
-                      start,
-                      1.0,
-                      curve: Curves.fastOutSlowIn,
+
+    return !animeDone
+        ? AnimatedBuilder(
+            animation: _controller,
+            builder: (BuildContext context, Widget child) {
+              return Stack(
+                children: <Widget>[
+                  _Animated(
+                    scale: Tween<Offset>(
+                            begin: begin / 100, end: (widget.offset) / 100)
+                        .animate(
+                      CurvedAnimation(
+                        parent: _controller,
+                        curve: Interval(
+                          start,
+                          1.0,
+                          curve: Curves.fastOutSlowIn,
+                        ),
+                      ),
+                    ),
+                    child: Container(
+                      key: _globalKey2,
+                      height: media.height * 0.1 + extraSize,
+                      width: media.width * 0.12 + extraSize,
+                      //  transform: Matrix4.identity()..rotateZ(_offset.value.dx*extraSize2),
+                      child: FlareActor(
+                        "assets/coin.flr",
+                        // animation: "rotate",
+                      ),
                     ),
                   ),
-                ),
-                child: Container(
-                  key: _globalKey2,
-                  height: media.height * 0.12 + extraSize,
-                  width: media.width * 0.12 + extraSize,
-                  // transform: Matrix4.identity()..rotateZ(extraSize*.05),
-                  child: FlareActor(
-                    "assets/coin.flr",
-                    animation: "rotate",
-                  ),
-                ),
-              ),
-            ],
+                ],
+              );
+            })
+        : new Container(
+            height: media.height * .15,
+            child: Center(
+                child: Text("Excellent",
+                    style: TextStyle(
+                        fontSize: 60.0, fontWeight: FontWeight.w900))),
           );
-        });
   }
 }
 
@@ -158,10 +179,8 @@ class _Animated extends AnimatedWidget {
     this.child,
     Animation<Offset> scale,
   }) : super(key: key, listenable: scale);
-  // TODO: implement listenable
   Animation<Offset> get listenable => super.listenable;
   double get translateX {
-    // print(listenable.value);
     double x = listenable.value.dx * 100;
     double val = x;
     // print(x);
@@ -189,10 +208,6 @@ class _Animated extends AnimatedWidget {
 
   @override
   Widget build(BuildContext context) {
-    // final Matrix4 mat =
-    //     new Matrix4.translationValues(translateX, translateY, 0.0);
-
-    // return Transform(transform: mat, child: child);
     return _SingleChild(
       child: child,
       offset: Offset(
@@ -226,6 +241,7 @@ class _RenderObject extends RenderProxyBox {
         _offset = offset,
         super(child);
   Offset _offset;
+
   set offsetUpdate(Offset of) {
     _offset = of;
     markNeedsPaint();
--- lib/screens/score_screen.dart
+++ lib/screens/score_screen.dart
@@ -83,23 +83,33 @@ class _ScoreScreenState extends State<ScoreScreen>
         duration: const Duration(milliseconds: 1000), vsync: this);
 
     buttoncontroller = new AnimationController(
-        duration: const Duration(milliseconds: 800), vsync: this);
+        duration: const Duration(milliseconds: 2000), vsync: this);
 
     _buttonAnimation =
         new CurvedAnimation(parent: buttoncontroller, curve: Curves.easeOut);
+
     _characterAnimation =
         new CurvedAnimation(parent: controller, curve: Curves.bounceOut);
 
-    final int totalDuration = 4000;
-    _animationController = AnimationController(
-        vsync: this, duration: new Duration(milliseconds: totalDuration));
-    animationDuration = totalDuration / (100 * (totalDuration / starCount));
-    _animationController.forward();
-    _animationController.addStatusListener((status) {
-      if (_animationController.isCompleted) {
+    // final int totalDuration = 4000;
+    // _animationController = AnimationController(
+    //     vsync: this, duration: new Duration(milliseconds: totalDuration));
+    // animationDuration = totalDuration / (100 * (totalDuration / starCount));
+    buttoncontroller.forward();
+
+    buttoncontroller.addStatusListener((status) {
+      if (buttoncontroller.isCompleted) {
+        setState(() {
+          // moveAnime = true;
+           buttoncontroller.reverse();
+        });
+      }
+      
+      if (buttoncontroller.value < 0.2) {
         setState(() {
           moveAnime = true;
-          coinCount = starCount;
+          // coinCount = starCount;
+          // buttoncontroller.reverse();
         });
       }
     });
@@ -210,23 +220,23 @@ class _ScoreScreenState extends State<ScoreScreen>
         duration: animationDuration);
   }
 
-  // buildFlareAnimation() {
-  //   new Future.delayed(Duration(milliseconds: 500), () {
-  //     setState(() {
-  //       moveAnime = true;
-  //       coinCount = starCount;
-  //     });
-  //   });
-  //   Size media = MediaQuery.of(context).size;
-  // return Container(
-  //   height: media.height * .1,
-  //   width: media.width * .1,
-  //   child: FlareActor(
-  //     "assets/coin.flr",
-  //     animation: "coin",
-  //   ),
-  // );
-  // }
+  buildFlareAnimation() {
+    // new Future.delayed(Duration(milliseconds: 500), () {
+    //   setState(() {
+    //     moveAnime = true;
+    //     coinCount = starCount;
+    //   });
+    // });
+    Size media = MediaQuery.of(context).size;
+    return Container(
+      height: media.height * .18,
+      width: media.width * .18,
+      child: FlareActor(
+        "assets/coin.flr",
+        animation: "coin",
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -506,10 +516,19 @@ class _ScoreScreenState extends State<ScoreScreen>
                         ? new Container(
                             height: ht * .15,
                             child: Center(
-                                child: Text("Excellent",
-                                    style: TextStyle(
-                                        fontSize: 60.0,
-                                        fontWeight: FontWeight.w900))),
+                              child: MoveContainer(
+                                  coinCount: coinCount,
+                                  index: inc,
+                                  offset: _offset,
+                                  starValue: starValues,
+                                  starCount: starCount,
+                                  animationController: _animationController,
+                                  duration: animationDuration),
+                              // Text("Excellent",
+                              //     style: TextStyle(
+                              //         fontSize: 60.0,
+                              //         fontWeight: FontWeight.w900))
+                            ),
                             // decoration: new BoxDecoration(
                             //   color: Colors.transparent,
                             //   borderRadius:
@@ -533,13 +552,16 @@ class _ScoreScreenState extends State<ScoreScreen>
                         : Container(),
                     !moveAnime
                         ? Container(
-                            child: Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: starValues
-                                    .map((e) => Padding(
-                                        padding: EdgeInsets.all(1.0),
-                                        child: _buildCoinItem(inc++, e)))
-                                    .toList(growable: false)),
+                            child: new ScaleTransition(
+                              scale: buttoncontroller,
+                              child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: starValues
+                                      .map((e) => Padding(
+                                          padding: EdgeInsets.all(1.0),
+                                          child: buildFlareAnimation()))
+                                      .toList(growable: false)),
+                            ),
                           )
                         : Container()
                     // Center(child: MoveContainer(coinCount: coinCount))
@@ -697,59 +719,44 @@ class _ScoreScreenState extends State<ScoreScreen>
 
                 // Icons which redirect to home, refresh and fast-forward
                 new Container(
-                    child: new ScaleTransition(
-                        scale: buttoncontroller,
-                        child: new Row(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            // IconButton(
-                            //     icon: new Image.asset("assets/home_button.png"),
-                            //     iconSize: ht > wd ? ht * 0.1 : wd * 0.08,
-                            //     onPressed: () {
-                            //       setState(() {
-                            //         _pageExited = true;
-                            //       });
+                    child: new Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Center(
+                        child: InkWell(
+                      onTap: () {
+                        setState(() {
+                          _pageExited = true;
+                        });
 
-                            //       AppStateContainer.of(context)
-                            //           .play('_audiotap.mp3');
-                            //       if (flag == true) {
-                            //         Navigator.of(context)
-                            //             .popUntil(ModalRoute.withName('/tab'));
-                            //       }
-                            //     }),
-                            Center(
-                                child: InkWell(
-                              onTap: () {
-                                setState(() {
-                                  _pageExited = true;
-                                });
-                                AppStateContainer.of(context)
-                                    .play('_audiotap.mp3');
-                                if (flag == true) {
-                                  Navigator.of(context)
-                                      .popUntil(ModalRoute.withName('/tab'));
-                                }
-                              },
-                              child: Container(
-                                height: ht * .07,
-                                width: wd * .3,
-                                margin: EdgeInsets.only(bottom: 10.0),
-                                decoration: new BoxDecoration(
-                                  color: Colors.orange,
-                                  border: new Border.all(
-                                      color: Colors.white, width: 2.0),
-                                  borderRadius: BorderRadius.circular(10.0),
-                                ),
-                                child: Center(child: Text("Next", style: new TextStyle(
-                                          fontSize:
-                                              ht > wd ? ht * 0.03 : wd * 0.03,
-                                          fontWeight: FontWeight.bold,
-                                          color: Colors.white))),
-                              ),
-                            ))
-                          ],
-                        ))),
+                        AppStateContainer.of(context).play('_audiotap.mp3');
+
+                        if (flag == true) {
+                          Navigator.of(context)
+                              .popUntil(ModalRoute.withName('/tab'));
+                        }
+                      },
+                      child: Container(
+                        height: ht * .07,
+                        width: wd * .3,
+                        margin: EdgeInsets.only(bottom: 10.0),
+                        decoration: new BoxDecoration(
+                          color: Colors.orange,
+                          border:
+                              new Border.all(color: Colors.white, width: 2.0),
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        child: Center(
+                            child: Text("Next",
+                                style: new TextStyle(
+                                    fontSize: ht > wd ? ht * 0.03 : wd * 0.03,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white))),
+                      ),
+                    ))
+                  ],
+                )),
               ],
             )));
       }),
