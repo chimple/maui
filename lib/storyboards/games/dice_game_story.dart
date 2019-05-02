4e2a91b83ab8a76c4a9f290ebdb65d8bd0fae9eb
Manukumar S B <manu@sutara.org>
2019-05-02 17:05:32 +0530

    dice game added

--- lib/storyboards/games/dice_game_story.dart
+++ lib/storyboards/games/dice_game_story.dart
@@ -10,9 +10,7 @@ class DiceGameStory extends FullScreenStory {
         Scaffold(
           body: SafeArea(
             child: DiceGame(
-              question: 4,
-              answerPosition: 1,
-              choices: BuiltList<int>([1, 4, 5, 8]),
+              choices: BuiltList<int>([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]),
               onGameOver: (_) {},
             ),
           ),
