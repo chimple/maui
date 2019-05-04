1302aa655f21c69dfbd9c7817e7ce5fd808490c4
satish-chimple <36834824+satish754ss@users.noreply.github.com>
2019-05-04 11:55:28 +0530

    Update jumble_words_game_story.dart

--- lib/storyboards/games/jumble_words_game_story.dart
+++ lib/storyboards/games/jumble_words_game_story.dart
@@ -1,6 +1,6 @@
 import 'package:built_collection/built_collection.dart';
 import 'package:flutter/material.dart';
-import 'package:maui/jamaica/games/moving_text_game .dart';
+import 'package:maui/jamaica/games/moving_text_game.dart';
 import 'package:storyboard/storyboard.dart';
 
 class JumbleWordsGameStory extends FullScreenStory {
